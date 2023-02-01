43cd0e9168c83fdb828500c401ee11a194129e2e
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-02 01:41:38 +0530

    Signup feature added in UserController

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,10 +1,17 @@
-const User = require('../models/UserModel')
+const User = require('../models/UserModel');
 
-
-const signin = async(req,res) => {
-    try {
-        const user = new User(req.body).save()
-    } catch (error) {
-        res.status(400).send('Invalid data or invalid syntax');
+const signin = async (req, res) => {
+  try {
+    const isExist = await User.findOne({email: req.body.email});
+    if (isExist) {
+      res.status(409).send('Email is already exist');
+    } else {
+      const user = await new User(req.body).save();
+      res.status(200).send(`${user.name} registered successfully`);
     }
-}
+  } catch (error) {
+    res.status(400).send('Invalid data or invalid syntax');
+  }
+};
+
+module.exports = {signin};
