ca6decf6c3f7f60748627458a09eadc0e3ae7d58
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-01 12:36:29 +0530

    Password hashing added to user schema

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+require('dotenv').config();
 
+// Creating user schema
 const UserSchema = mongoose.Schema({
   first_name: {type: String, required: true},
   last_name: {type: String, required: true},
@@ -14,6 +18,31 @@ const UserSchema = mongoose.Schema({
     lowercase: true,
     trim: true,
   },
-  mobile_number: {type: Number, required: true, unique: true},
+  mobile_number: {
+    type: Number,
+    required: true,
+    unique: true,
+    minlength: [10, 'Please enter 10 digit mobile number'],
+    maxlength: [10, 'Please enter 10 digit mobile number'],
+  },
+  password: {
+    type: String,
+    trim: true,
+    minlength: [8, 'Minimum length of password should be 8'],
+  },
   token: {type: String},
 });
+
+// Password hashing
+UserSchema.pre('save', async function (next) {
+  try {
+    if (this.isModified('password')) {
+      this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+
