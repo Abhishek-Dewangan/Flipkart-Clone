d9ed4dbc9710777fcd57015d05082b126fe4a232
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-07 00:17:59 +0530

    Wishlist model created

---
+++ server/models/WishlishModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const Wishlist = new mongoose.Schema({
+  name: {type: String},
+  category: {type: String},
+  link: {type: String},
+  current_price: {type: Number},
+  original_price: {type: Number},
+  discounted: {type: Boolean},
+  thumbnail: {type: String},
+  query_url: {type: String},
+});
+
+module.exports = mongoose.model('wishlist', Wishlist);
