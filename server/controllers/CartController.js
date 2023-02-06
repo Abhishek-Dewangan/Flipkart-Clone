c0bf0fd621aa568d0f068f2868ec38d817a989e3
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-06 23:18:43 +0530

    Add to cart fuction added in cart controller

--- server/controllers/CartController.js
+++ server/controllers/CartController.js
@@ -1,6 +1,22 @@
 const Cart = require('../models/CartModel');
 
-
+// Adding product into cart
+const addToCart = async (req, res) => {
+  try {
+    const {_id} = req.body;
+    const isProductExist = await Cart.findById({_id});
+    if (isProductExist) {
+      res.status(409).send({message: 'Product already added into cart'});
+    } else {
+      const response = await new Cart(req.body).save();
+      res
+        .status(201)
+        .send({message: 'Product successfully added into cart', response});
+    }
+  } catch (error) {
+    res.status(401).send({message: 'Unable to add product into cart', error});
+  }
+};
 
 
 
