07eb109633807d34f9668b00615e95edbfcfb305
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-09 19:43:12 +0530

    Add to wishlist fuction added in wishlist controller

--- server/controllers/WishlistController.js
+++ server/controllers/WishlistController.js
@@ -1,2 +1,21 @@
 Wishlist = require('../models/WishlishModel');
 
+// Adding product in whishlist
+const addToWishlist = async (req, res) => {
+  try {
+    const {_id} = req.body;
+    const isExist = await Wishlist.findById({_id});
+    if (isExist) {
+      res.status(409).send({message: 'Product is already exist in wishlist'});
+    } else {
+      const response = await Wishlist(req.body).save();
+      res
+        .status(201)
+        .send({message: 'Product successfully added into wishlist', response});
+    }
+  } catch (error) {
+    res
+      .status(401)
+      .send({message: 'Unable to add producut on wishlist', error});
+  }
+};
