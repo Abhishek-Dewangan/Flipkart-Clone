26682e06a8d28598f20dc27472c8eca5ac024853
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-10 20:03:05 +0530

    Remove all products from cart function added in cart controller

--- server/controllers/CartController.js
+++ server/controllers/CartController.js
@@ -34,13 +34,27 @@ const addToCart = async (req, res) => {
 const removeFromCart = async (req, res) => {
   try {
     const {id} = req.params;
-    const removeItem = await Cart.findByIdAndDelete({_id: id});
+    const response = await Cart.findByIdAndDelete({_id: id});
     res
       .status(201)
-      .send({message: 'Product successfully removed from cart', removeItem});
+      .send({message: 'Product successfully removed from cart', response});
   } catch (error) {
     res.status(401).send({message: 'Unable to remove product', error});
   }
 };
 
-module.exports = {getCartProducts, addToCart, removeFromCart};
+// Remove all products from cart
+const removeAllFromCart = async (req, res) => {
+  try {
+    const response = await Cart.remove({});
+    res
+      .status(201)
+      .send({message: 'All products successfully removed from cart', response});
+  } catch (error) {
+    res
+      .status(401)
+      .send({message: 'Unable to remove all products from cart', error});
+  }
+};
+
+module.exports = {getCartProducts, addToCart, removeFromCart, removeAllFromCart};
