9d83d84988fe487c7dcfbbe06f21344cefbaa6de
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-11 13:41:10 +0530

    Get address function added in address controller

--- server/controllers/AddressController.js
+++ server/controllers/AddressController.js
@@ -1,5 +1,16 @@
 const Address = require('../models/AddressModel');
 
+const getAddress = async (req, res) => {
+  try {
+    const {userId} = req.params;
+    const addresses = await Address.find({userId: userId});
+    res.status(201).send({message: 'Successfully got the addres', addresses});
+    res.send;
+  } catch (error) {
+    res.status(401).send({message: 'Unable to get address', error});
+  }
+};
+
 const addNewAddress = async (req, res) => {
   try {
     const response = await new Address(req.body).save();
@@ -8,3 +19,5 @@ const addNewAddress = async (req, res) => {
     res.status(401).send({message: 'Unable to add new address', error});
   }
 };
+
+module.exports = {addNewAddress, getAddress};
