66f87ca4ccf715f9837d92c7949d57ed099de4ea
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-11 13:25:55 +0530

    Address schema created

--- server/models/AddressModel.js
+++ server/models/AddressModel.js
@@ -1,2 +1,16 @@
 const mongoose = require('mongoose');
 
+const AddressSchema = mongoose.Schema({
+  userId: {type: String, required: true},
+  name: {type: String, required: true},
+  number: {type: String, required: true},
+  pincode: {type: String, required: true},
+  locality: {type: String},
+  houseAddress: {type: String, required: true},
+  city: {type: String, required: true},
+  state: {type: String, required: true},
+  addressType: {type: String},
+  landmark: {type: String},
+});
+
+module.exports = mongoose.model('address', AddressSchema);
