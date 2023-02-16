dc5212fa7d58d4f69e8ed51637511c47539f3b7d
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-17 00:29:17 +0530

    Header has well style now

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -3,28 +3,46 @@ import styles from './Header.module.css';
 import {Link} from 'react-router-dom';
 import {SiFlipkart} from 'react-icons/si';
 import {BiSearch, BiChevronDown, BiChevronUp} from 'react-icons/bi';
+import {FaShoppingCart} from 'react-icons/fa';
 
 const Header = () => {
   const [open, setOpen] = useState(false);
   return (
     <nav className={styles.navbar}>
-      <SiFlipkart className={styles.flipkartIcon} />
-      <div className={styles.searchBar}>
-        <input
-          type={'text'}
-          name={'searchBar'}
-          placeholder={'Search for products, brand and more'}
-        />
-        <BiSearch color='#2455f4' />
-      </div>
-      <button>Login</button>
-      <Link>Become a Seller</Link>
-      <Link
-        onMouseEnter={() => setOpen(!open)}
-        onMouseDown={() => setOpen(!open)}
-      >
-        More {open ? <BiChevronUp /> : <BiChevronDown />}
-      </Link>
+      <section className={styles.left}>
+        <Link className={styles.link}>
+          <SiFlipkart className={styles.flipkartIcon} />
+        </Link>
+        <div className={styles.searchBar}>
+          <input
+            type={'text'}
+            name={'searchBar'}
+            placeholder={'Search for products, brand and more'}
+          />
+          <BiSearch className={styles.searchIcon} />
+        </div>
+        <button className={styles.loginBtn}>Login</button>
+      </section>
+      <section className={styles.right}>
+        <Link className={styles.link}>Become a Seller</Link>
+        <Link
+          className={styles.link}
+          onMouseEnter={() => setOpen(!open)}
+          onMouseLeave={() => setOpen(!open)}
+        >
+          <div className={styles.more}>
+            More
+            <label className={styles.openCloseIcon}>
+              {open ? <BiChevronUp /> : <BiChevronDown />}
+            </label>
+          </div>
+        </Link>
+        <Link className={styles.link}>
+          <div className={styles.cart}>
+            <FaShoppingCart className={styles.cartIcon} /> Cart
+          </div>
+        </Link>
+      </section>
     </nav>
   );
 };
