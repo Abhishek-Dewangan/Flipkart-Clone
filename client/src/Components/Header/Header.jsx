9f9b61a030b732ad3e7b1b9e5ff2edf085bbe37a
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-16 21:07:39 +0530

    Add flipkart icon and search bar with login button in header

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react';
 import styles from './Header.module.css';
+import {Link} from 'react-router-dom';
+import {SiFlipkart} from 'react-icons/si';
+import {BiSearch, BiChevronDown, BiChevronUp} from 'react-icons/bi';
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <div>Header</div>
-  )
-}
+    <nav className={styles.navbar}>
+      <SiFlipkart className={styles.flipkartIcon} />
+      <div className={styles.searchBar}>
+        <input
+          type={'text'}
+          name={'searchBar'}
+          placeholder={'Search for products, brand and more'}
+        />
+        <BiSearch color='#2455f4' />
+      </div>
+      <button>Login</button>
+      <Link>Become a Seller</Link>
+      <Link
+        onMouseEnter={() => setOpen(!open)}
+        onMouseDown={() => setOpen(!open)}
+      >
+        More {open ? <BiChevronUp /> : <BiChevronDown />}
+      </Link>
+    </nav>
+  );
+};
 
-export default Header
+export default Header;
