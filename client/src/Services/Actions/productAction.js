8d3469ee06f700d833a6a911fce45c2669ccca7c
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-20 22:29:08 +0530

    Get product function added in product action

--- client/src/Services/Actions/productAction.js
+++ client/src/Services/Actions/productAction.js
@@ -3,5 +3,16 @@ import axios from 'axios';
 // Actions types
 export const GET_PRODUCTS = 'GET_PRODUCTS';
 export const GET_PRODUCTS_BY_ID = 'GET_PRODUCTS_BY_ID';
-export const GET_PRODUCTS_BY_CATEGORY = 'GET_PRODUCTS_BY_CATEGORY'
+export const GET_PRODUCTS_BY_CATEGORY = 'GET_PRODUCTS_BY_CATEGORY';
+export const IS_ERROR = 'IS_ERROR';
+export const IS_SUCCESS = 'IS_SUCCESS';
 
+// Get products
+export const getPrducts = async (dispatch) => {
+  try {
+    const products = await axios.get('http://localhost/api/getproducts');
+    dispatch({type: IS_SUCCESS, payload: products});
+  } catch (error) {
+    dispatch({type: IS_ERROR, payload: error});
+  }
+};
