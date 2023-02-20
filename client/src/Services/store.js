2b854b2194bef44cdf6053ae02a210c3085191d2
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-20 22:11:14 +0530

    React redux setup completed

--- client/src/Services/store.js
+++ client/src/Services/store.js
@@ -1,5 +1,5 @@
-import {legacy_createStore as createStore, combineReducer} from 'react-redux';
+import {legacy_createStore as createStore, combineReducers} from 'redux';
 
-const rootReducer = combineReducer({});
+const rootReducer = combineReducers({});
 
 export const store = createStore(rootReducer);
