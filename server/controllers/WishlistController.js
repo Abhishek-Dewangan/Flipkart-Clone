07ec45b13124bd80b35967d87b0da6362307b78b
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-20 12:01:25 +0530

    Add to wishlist function error solved

--- server/controllers/WishlistController.js
+++ server/controllers/WishlistController.js
@@ -16,20 +16,18 @@ const getWishlistProducts = async (req, res) => {
 // Adding product in whishlist
 const addToWishlist = async (req, res) => {
   try {
-    const {_id} = req.body;
-    const isExist = await Wishlist.findById({_id});
-    if (isExist) {
+    const {userId, productId} = req.body;
+    const isProductExist = await Wishlist.findOne({userId, productId});
+    if (isProductExist) {
       res.status(409).send({message: 'Product is already exist in wishlist'});
     } else {
-      const response = await Wishlist(req.body).save();
+      const response = await new Wishlist(req.body).save();
       res
         .status(201)
         .send({message: 'Product successfully added into wishlist', response});
     }
   } catch (error) {
-    res
-      .status(401)
-      .send({message: 'Unable to add producut on wishlist', error});
+    res.status(401).send({message: error.message, error});
   }
 };
 
@@ -40,7 +38,7 @@ const removeFromWishlist = async (req, res) => {
     const response = await Wishlist.findByIdAndDelete({_id: id});
     res
       .status(201)
-      .send({message: 'Product removed successfully from wishlist',response});
+      .send({message: 'Product removed successfully from wishlist', response});
   } catch (error) {
     res
       .status(401)
@@ -49,13 +47,23 @@ const removeFromWishlist = async (req, res) => {
 };
 
 // Remove all products from wishlist
-const removeAllFromWishlist = async(req,res) => {
+const removeAllFromWishlist = async (req, res) => {
   try {
     const response = await Wishlist.remove({});
-    res.status(201).send({message:'All products removed from wishlist',response});
+    res
+      .status(201)
+      .send({message: 'All products removed from wishlist', response});
   } catch (error) {
-    res.status(401).send({message:'Unabe to remove all products from wishlist',error});
+    res
+      .status(401)
+      .send({message: 'Unabe to remove all products from wishlist', error});
   }
-}
+};
 
-module.exports = {getWishlistProducts, addToWishlist, removeFromWishlist, removeAllFromWishlist};
+// Exporting wishlist routes
+module.exports = {
+  getWishlistProducts,
+  addToWishlist,
+  removeFromWishlist,
+  removeAllFromWishlist,
+};
