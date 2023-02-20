4cb6fa15cfb386536df7efb65bfe555f88708ac2
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-20 22:23:54 +0530

    Peoduct action created

---
+++ client/src/Services/Actions/productAction.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+// Actions types
+export const GET_PRODUCTS = 'GET_PRODUCTS';
+export const GET_PRODUCTS_BY_ID = 'GET_PRODUCTS_BY_ID';
+export const GET_PRODUCTS_BY_CATEGORY = 'GET_PRODUCTS_BY_CATEGORY'
+
