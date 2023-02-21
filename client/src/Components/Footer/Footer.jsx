2a70265735c938382eb3af640f9d058dcc97c77e
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-21 16:03:04 +0530

    Footer bottom section completed with good styles and updated with links

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import styles from './Footer.module.css';
 import {about, help, policy, social} from '../../Assets/FooterData/FooterData';
+import {BsBagFill} from 'react-icons/bs';
+import {GiAlliedStar, GiHelp} from 'react-icons/gi';
+import {MdCardGiftcard} from 'react-icons/md';
+import {
+  FaCcAmex,
+  FaCcVisa,
+  FaCcMastercard,
+  FaCcAmazonPay,
+  FaCcApplePay,
+  FaCcPaypal,
+} from 'react-icons/fa';
+import {SiMastercard} from 'react-icons/si';
+import {SlCreditCard} from 'react-icons/sl';
 
 const Footer = () => {
   return (
@@ -10,7 +23,9 @@ const Footer = () => {
           <span>ABOUT</span>
           {about.map((elem, i) => (
             <p key={i}>
-              <a href={elem.link}>{elem.name}</a>
+              <a href={elem.link} target={'_blank'} rel='noreferrer'>
+                {elem.name}
+              </a>
             </p>
           ))}
         </div>
@@ -18,7 +33,9 @@ const Footer = () => {
           <span>HELP</span>
           {help.map((elem, i) => (
             <p key={i}>
-              <a href={elem.link}>{elem.name}</a>
+              <a href={elem.link} target={'_blank'} rel='noreferrer'>
+                {elem.name}
+              </a>
             </p>
           ))}
         </div>
@@ -26,7 +43,9 @@ const Footer = () => {
           <span>POLICY</span>
           {policy.map((elem, i) => (
             <p key={i}>
-              <a href={elem.link}>{elem.name}</a>
+              <a href={elem.link} target={'_blank'} rel='noreferrer'>
+                {elem.name}
+              </a>
             </p>
           ))}
         </div>
@@ -34,30 +53,81 @@ const Footer = () => {
           <span>SOCIAL</span>
           {social.map((elem, i) => (
             <p key={i}>
-              <a href={elem.link}>{elem.name}</a>
+              <a href={elem.link} target={'_blank'} rel='noreferrer'>
+                {elem.name}
+              </a>
             </p>
           ))}
         </div>
-        <hr className={styles.horizonLine} />
+        <hr className={styles.verticleLine} />
         <div className={styles.linkSection}>
           <span>Mail Us:</span>
-          <p>
+          <text>
             Flipkart Internet Private Limited, Buildings Alyssa, Begonia & Clove
             Embassy Tech Village, Outer Ring Road, Devarabeesanahalli Village,
             Bengaluru, 560103, Karnataka, India
-          </p>
+          </text>
         </div>
         <div className={styles.linkSection}>
           <span>Registered Ofiice Address:</span>
-          <p>
+          <text>
             Flipkart Internet Private Limited, Buildings Alyssa, Begonia & Clove
             Embassy Tech Village, Outer Ring Road, Devarabeesanahalli Village,
             Bengaluru, 560103, Karnataka, India CIN : U51109KA2012PTC066107
             Telephone: 044-45614700
-          </p>
+          </text>
         </div>
       </section>
-      <section className={styles.bottomSection}></section>
+      <hr className={styles.horizonLine} />
+      <section className={styles.bottomSection}>
+        <p>
+          <a
+            href='https://seller.flipkart.com/?utm_source=fkwebsite&utm_medium=websitedirect'
+            target={'_blank'}
+            rel='noreferrer'
+          >
+            <BsBagFill className={styles.icon} /> Become a Seller
+          </a>
+        </p>
+        <p>
+          <a
+            href='https://brands.flipkart.com/'
+            target={'_blank'}
+            rel='noreferrer'
+          >
+            <GiAlliedStar className={styles.icon} /> Advertise
+          </a>
+        </p>
+        <p>
+          <a
+            href='https://www.flipkart.com/the-gift-card-store?otracker=undefined_footer_footer_navlinks'
+            target={'_blank'}
+            rel='noreferrer'
+          >
+            <MdCardGiftcard className={styles.icon} /> Gift Card
+          </a>
+        </p>
+        <p>
+          <a
+            href='https://www.flipkart.com/helpcentre?otracker=undefined_footer_footer_navlinks'
+            target={'_blank'}
+            rel='noreferrer'
+          >
+            <GiHelp className={styles.icon} /> Help Center
+          </a>
+        </p>
+        <p>© 2007-2023 Flipkart.com</p>
+        <p className={styles.cards}>
+          <FaCcVisa className={styles.card} />
+          <FaCcMastercard className={styles.card} />
+          <SiMastercard className={styles.card} />
+          <SlCreditCard className={styles.card} />
+          <FaCcAmazonPay className={styles.card} />
+          <FaCcApplePay className={styles.card} />
+          <FaCcPaypal className={styles.card} />
+          <FaCcAmex className={styles.card} />
+        </p>
+      </section>
     </footer>
   );
 };
