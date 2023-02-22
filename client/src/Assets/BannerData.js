3080fb65b4575c2594c2eefe17a8fa515a5f27e4
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-22 12:08:46 +0530

    Banner data with images and link created

--- client/src/Assets/BannerData.js
+++ client/src/Assets/BannerData.js
@@ -1,1 +1,17 @@
-export const BannerData = []
+import appliance from './Images/appliance.jpg';
+import babycare from './Images/babycare.jpg';
+import beauty from './Images/beauty.jpg';
+import growcery from './Images/growcery.jpg';
+import home from './Images/home.jpg';
+import mobile from './Images/mobile.jpg';
+import sports from './Images/sports.jpg';
+
+export const BannerData = [
+  {banner: growcery, link: '/growcery'},
+  {banner: mobile, link: '/mobile'},
+  {banner: appliance, link: '/appliance'},
+  {banner: home, link: '/home'},
+  {banner: babycare, link: '/babycare'},
+  {banner: beauty, link: '/beauty'},
+  {banner: sports, link: 'sports'},
+];
