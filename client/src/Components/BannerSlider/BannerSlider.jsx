4bc1127858ff81f6255e8e4a1fa4a43f88615a17
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-25 00:41:46 +0530

    Added carousel feature in Banner slider

--- client/src/Components/BannerSlider/BannerSlider.jsx
+++ client/src/Components/BannerSlider/BannerSlider.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import styles from './BannerSlider.module.css';
+import {BannerData} from '../../Assets/BannerData';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import {Navigation, Pagination, Scrollbar, A11y} from 'swiper';
+import "swiper/css";
+import "swiper/css/pagination";
 
 const BannerSlider = () => {
   return (
-    <div>
-    </div>
+    <Swiper
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      pagination={{clickable: true}}
+    >
+      {BannerData.map((elem, i) => {
+        return (
+          <SwiperSlide>
+            <img
+              src={elem.banner}
+              alt='Banner'
+              className={styles.bannerImage}
+            />
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
   );
 };
 
