89967a172f54861c1ac2b21c55d4411c875a0f8d
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-25 20:18:59 +0530

    Product category page is created with style file

---
+++ client/src/Pages/ProductCategoryPage/ProductCategoryPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './ProductCategoryPage.module.css'
+
+const ProductCategoryPage = () => {
+  return (
+    <div>ProductCategoryPage</div>
+  )
+}
+
+export default ProductCategoryPage
