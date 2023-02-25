e081eefcf69ab0fbcf26ded1298d8e4eb4f7f7ed
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-25 18:15:57 +0530

    Banner work is completed with autoslide and navigation featured through react slider

--- client/src/Components/BannerSlider/BannerSlider.jsx
+++ client/src/Components/BannerSlider/BannerSlider.jsx
@@ -2,20 +2,31 @@ import React, {useRef} from 'react';
 import styles from './BannerSlider.module.css';
 import {BannerData} from '../../Assets/BannerData';
 import {Swiper, SwiperSlide} from 'swiper/react';
-import {Navigation, Pagination, Scrollbar, A11y} from 'swiper';
+import SwiperCore, {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Autoplay,
+} from 'swiper';
 import 'swiper/css';
 import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import 'swiper/css/autoplay';
 
 const BannerSlider = () => {
+  SwiperCore.use([Autoplay]);
   return (
     <Swiper
       modules={[Navigation, Pagination, Scrollbar, A11y]}
       pagination={{clickable: true}}
       autoplay={{delay: 2500, disableOnInteraction: false}}
+      navigation
+      className={styles.bannerSliderContainer}
     >
       {BannerData.map((elem, i) => {
         return (
-          <SwiperSlide>
+          <SwiperSlide key={i}>
             <img
               src={elem.banner}
               alt='Banner'
