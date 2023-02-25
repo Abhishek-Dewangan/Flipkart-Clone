a23f8cbc0955db617def4d1edd7454239f4f7479
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-25 18:53:28 +0530

    Subheader data has been mapped in subheader file

--- client/src/Components/SubHeader/SubHeader.jsx
+++ client/src/Components/SubHeader/SubHeader.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-import styles from './SubHeader.module.css'
-import {Link} from 'react-router-dom'
+import React from 'react';
+import styles from './SubHeader.module.css';
+import {Link} from 'react-router-dom';
+import {subHeaderData} from '../../Assets/SubHeaderData';
 
 const SubHeader = () => {
   return (
-    <div className={styles.subHeaderContainer}>SubHeader</div>
-  )
-}
+    <div className={styles.subHeaderContainer}>
+      {subHeaderData.map((elem, i) => {
+        return (
+          <div key={i}>
+            <Link>
+              <img src={elem.image} alt={elem.name} />
+              <p>{elem.name}</p>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export default SubHeader
+export default SubHeader;
