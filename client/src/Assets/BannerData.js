96aec75f977aed908e73a06ff7e8cf51c22a1080
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-26 00:44:13 +0530

    Add some extra banner image in banner data

--- client/src/Assets/BannerData.js
+++ client/src/Assets/BannerData.js
@@ -2,10 +2,16 @@ import image1 from './BannerImages/image1.webp';
 import image2 from './BannerImages/image2.webp';
 import image3 from './BannerImages/image3.webp';
 import image4 from './BannerImages/image4.webp';
+import image5 from './BannerImages/image5.png';
+import image6 from './BannerImages/image6.png';
+import image7 from './BannerImages/image7.png';
 
 export const bannerData = [
-  {banner: image4, link: '/home'},
-  {banner: image1, link: '/growcery'},
-  {banner: image2, link: '/mobile'},
-  {banner: image3, link: '/appliance'},
+  {banner: image4, linkName: 'mobile'},
+  {banner: image1, linkName: 'appliance'},
+  {banner: image2, linkName: 'mobile'},
+  {banner: image3, linkName: 'mobile'},
+  {banner: image5, linkName: 'mobile'},
+  {banner: image6, linkName: 'growcery'},
+  {banner: image7, linkName: 'home'},
 ];
