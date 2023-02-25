30e38e82449c5f312e8309e6af41cf87643e04eb
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-25 20:14:34 +0530

    Banner data update

--- client/src/Assets/BannerData.js
+++ client/src/Assets/BannerData.js
@@ -4,8 +4,8 @@ import image3 from './BannerImages/image3.webp';
 import image4 from './BannerImages/image4.webp';
 
 export const bannerData = [
+  {banner: image4, link: '/home'},
   {banner: image1, link: '/growcery'},
   {banner: image2, link: '/mobile'},
   {banner: image3, link: '/appliance'},
-  {banner: image4, link: '/home'},
 ];
