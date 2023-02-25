c689e68b1b8f08c980c5bbd59451c3943cf30014
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-25 16:55:19 +0530

    Update cart schema

--- server/models/CartModel.js
+++ server/models/CartModel.js
@@ -2,7 +2,14 @@ const mongoose = require('mongoose');
 
 const CartSchema = new mongoose.Schema({
   userId: {type: String, required: true},
-  productId: {type: String, required: true},
+  name: {type: String},
+  category: {type: String},
+  link: {type: String},
+  current_price: {type: Number},
+  original_price: {type: Number},
+  discounted: {type: Boolean},
+  thumbnail: {type: String},
+  query_url: {type: String},
 });
 
 module.exports = mongoose.model('cart', CartSchema);
