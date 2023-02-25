9c2c5d2cfe3cc3263d39c8903e829b94bed9fabd
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-25 17:57:04 +0530

    Banner images are replaced with good quality images

--- client/src/Assets/BannerData.js
+++ client/src/Assets/BannerData.js
@@ -1,17 +1,11 @@
-import appliance from './BannerImages/appliance.jpg';
-import babycare from './BannerImages/babycare.jpg';
-import beauty from './BannerImages/beauty.jpg';
-import growcery from './BannerImages/growcery.jpg';
-import home from './BannerImages/home.jpg';
-import mobile from './BannerImages/mobile.jpg';
-import sports from './BannerImages/sports.jpg';
+import image1 from './BannerImages/image1.webp';
+import image2 from './BannerImages/image2.webp';
+import image3 from './BannerImages/image3.webp';
+import image4 from './BannerImages/image4.webp';
 
 export const BannerData = [
-  {banner: growcery, link: '/growcery'},
-  {banner: mobile, link: '/mobile'},
-  {banner: appliance, link: '/appliance'},
-  {banner: home, link: '/home'},
-  {banner: babycare, link: '/babycare'},
-  {banner: beauty, link: '/beauty'},
-  {banner: sports, link: 'sports'},
+  {banner: image1, link: '/growcery'},
+  {banner: image2, link: '/mobile'},
+  {banner: image3, link: '/appliance'},
+  {banner: image4, link: '/home'},
 ];
