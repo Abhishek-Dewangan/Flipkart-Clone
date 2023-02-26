cd88d4e1e0bbab10adba8b8b8c0c23da336be0ae
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-27 00:04:12 +0530

    Cart reducer is created with all action cases

--- client/src/Services/Reducers/cartReducer.js
+++ client/src/Services/Reducers/cartReducer.js
@@ -5,3 +5,41 @@ import {
   REMOVE_ALL_FROM_CART,
   REMOVE_FROM_CART,
 } from '../Actions/CartAction';
+
+const initialState = {
+  cartData: [],
+  isLoading: false,
+  isError: false,
+};
+
+export const CartReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TO_CART: {
+      return {
+        ...state,
+        cartData: [...state.cartData, action.payload],
+      };
+    }
+    case GET_CART_DATA: {
+      return {
+        ...state,
+        cartData: action.payload,
+      };
+    }
+    case REMOVE_FROM_CART: {
+      return {
+        ...state,
+        cartData: action.payload,
+      };
+    }
+    case REMOVE_ALL_FROM_CART: {
+      return {
+        ...state,
+        cartData: [],
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
