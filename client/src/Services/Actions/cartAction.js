3580ae8ef4d049683a1ca45379391e1705147a57
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-26 23:38:54 +0530

    All action type of cart is created and expoted

---
+++ client/src/Services/Actions/cartAction.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+// Actions type
+export const ADD_TO_CART = 'ADD_TO_CART';
+export const GET_CART_DATA = 'GET_CART_DATA';
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+export const RAMOVE_ALL_FROM_CART = 'RAMOVE_ALL_FROM_CART';
