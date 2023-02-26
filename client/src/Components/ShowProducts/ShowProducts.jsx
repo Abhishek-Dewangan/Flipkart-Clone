79700d740d503e03b04b8f211515d2430b3c65f7
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-26 23:04:27 +0530

    Show product component is completed with good style

--- client/src/Components/ShowProducts/ShowProducts.jsx
+++ client/src/Components/ShowProducts/ShowProducts.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './ShowProducts.module.css';
+import fAssured from '../../Assets/Images/f-assured.png';
 
 const ShowProducts = ({products}) => {
   console.log(products);
@@ -8,15 +9,24 @@ const ShowProducts = ({products}) => {
       {products.map((elem) => {
         return (
           <div key={elem._id} className={styles.productBox}>
-            <img
-              src={elem.thumbnail}
-              alt={elem.category}
-              className={styles.productImage}
-            />
+            <div classname={styles.productImageDiv}>
+              <img
+                src={elem.thumbnail}
+                alt={elem.category}
+                className={styles.productImage}
+              />
+            </div>
             <p className={styles.productName}>{elem.name}</p>
-            <p>
+            <img
+              className={styles.fAssured}
+              src={fAssured}
+              alt='Flipkart assured'
+            />
+            <p className={styles.price}>
               <span className={styles.currentPrice}>₹{elem.current_price}</span>{' '}
-              <strike className={styles.originalPrice}>₹{elem.original_price}</strike>
+              <strike className={styles.originalPrice}>
+                ₹{elem.original_price}
+              </strike>
             </p>
           </div>
         );
