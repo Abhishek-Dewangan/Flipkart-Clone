35683dab698ffcd266a686f7ffa2515b4c8dab30
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-26 11:47:47 +0530

    Category products are mapped in show prouduct component

--- client/src/Components/ShowProducts/ShowProducts.jsx
+++ client/src/Components/ShowProducts/ShowProducts.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import styles from './ShowProducts.module.css';
 
 const ShowProducts = ({products}) => {
+  console.log(products);
   return (
-    <div>ShowProducts</div>
-  )
-}
+    <div className={styles.showProductsContainer}>
+      {products.map((elem) => {
+        return (
+          <div key={elem._id} className={styles.productBox}>
+            <img
+              src={elem.thumbnail}
+              alt={elem.category}
+              className={styles.productImage}
+            />
+            <p className={styles.productName}>{elem.name}</p>
+            <p>
+              <span className={styles.currentPrice}>₹{elem.current_price}</span>{' '}
+              <strike className={styles.originalPrice}>₹{elem.original_price}</strike>
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export default ShowProducts
+export default ShowProducts;
