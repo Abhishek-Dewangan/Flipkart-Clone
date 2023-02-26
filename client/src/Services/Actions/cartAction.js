a40d0ce81fd5178f22ed5350bfeb425aee68ba7c
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-26 23:42:52 +0530

    Add to cart fuction is created in cart action

--- client/src/Services/Actions/cartAction.js
+++ client/src/Services/Actions/cartAction.js
@@ -5,3 +5,16 @@ export const ADD_TO_CART = 'ADD_TO_CART';
 export const GET_CART_DATA = 'GET_CART_DATA';
 export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
 export const RAMOVE_ALL_FROM_CART = 'RAMOVE_ALL_FROM_CART';
+
+// Add to cart
+export const addToCart = async (dispatch, product) => {
+  try {
+    const res = await axios.post(
+      'http://localhost:8080/api/addtocart',
+      product
+    );
+    dispatch({type: ADD_TO_CART, payload: res});
+  } catch (error) {
+    dispatch({type: Error, payload: error});
+  }
+};
