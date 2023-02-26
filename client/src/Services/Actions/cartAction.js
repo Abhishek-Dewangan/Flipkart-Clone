3917f6eb688b8874a195abf9676fe231285d06eb
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-26 23:46:19 +0530

    Get cart data fuction added in cart action

--- client/src/Services/Actions/cartAction.js
+++ client/src/Services/Actions/cartAction.js
@@ -5,6 +5,7 @@ export const ADD_TO_CART = 'ADD_TO_CART';
 export const GET_CART_DATA = 'GET_CART_DATA';
 export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
 export const RAMOVE_ALL_FROM_CART = 'RAMOVE_ALL_FROM_CART';
+export const ERROR = 'ERROR';
 
 // Add to cart
 export const addToCart = async (dispatch, product) => {
@@ -15,6 +16,16 @@ export const addToCart = async (dispatch, product) => {
     );
     dispatch({type: ADD_TO_CART, payload: res});
   } catch (error) {
-    dispatch({type: Error, payload: error});
+    dispatch({type: ERROR, payload: error});
+  }
+};
+
+// Get cart data
+export const getCartData = async (dispatch) => {
+  try {
+    const cartData = await axios.get('http://localhost:8080/api/getcartdata');
+    dispatch({type: GET_CART_DATA, payload: cartData});
+  } catch (error) {
+    dispatch({type: ERROR, payload: error});
   }
 };
