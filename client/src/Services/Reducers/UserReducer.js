a087451b6b2fa321c5223d5292522ef391f4fca7
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-27 18:16:06 +0530

    Signout and signup case is added in user reducer

--- client/src/Services/Reducers/UserReducer.js
+++ client/src/Services/Reducers/UserReducer.js
@@ -7,13 +7,25 @@ const initialState = {
   user: {},
 };
 
-export const UserReducer = (state: initialState, action) => {
+export const UserReducer = (state = initialState, action) => {
   switch (action.type) {
     case SIGNIN: {
       return {
         ...state,
         token: action.payload.token,
         user: action.payload,
+      };
+    }
+    case SIGNUP: {
+      return {
+        ...state,
+      };
+    }
+    case SIGNOUT: {
+      return {
+        ...state,
+        token: '',
+        user: {},
       };
     }
     default: {
