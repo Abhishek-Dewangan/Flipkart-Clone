2dc5f5d0caa94b21ff0c5a2ed4d82696c3da3db3
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-27 17:50:40 +0530

    All user action type is created and exported

---
+++ client/src/Services/Actions/UserAction.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+// Action types
+export const SIGNUP = 'SIGNUP';
+export const SIGNIN = 'SIGNIN';
+export const SIGNOUT = 'SIGNOUT';
+export const IS_ERROR = 'IS_ERROR'
