110276d959b45abd10b2c63fef40e439ecf46007
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-27 17:58:20 +0530

    User signin fuction added in user action

--- client/src/Services/Actions/UserAction.js
+++ client/src/Services/Actions/UserAction.js
@@ -6,11 +6,21 @@ export const SIGNIN = 'SIGNIN';
 export const SIGNOUT = 'SIGNOUT';
 export const IS_ERROR = 'IS_ERROR';
 
-// Signup/Register
+// User Signup/Register
 export const signup = async (dispatch, user) => {
   try {
     const res = await axios.post(`http://localhost:8080/api/signup`, user);
     dispatch({type: SIGNUP, payload: res});
+  } catch (error) {
+    dispatch({type: IS_ERROR, payload: error});
+  }
+};
+
+// User Signin/Login
+export const signin = async (dispatch, user) => {
+  try {
+    const res = await axios.post(`http://localhost:8080/api/signin`, user);
+    dispatch({type: SIGNIN, payload: res});
   } catch (error) {
     dispatch({type: IS_ERROR, payload: error});
   }
