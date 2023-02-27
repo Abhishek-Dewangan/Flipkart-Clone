64ee730a47eaf4e5ef440849514217924aa98cc2
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-27 11:29:00 +0530

    Add to wishlist fuction is added in wishlist action

--- client/src/Services/Actions/WishlistAction.js
+++ client/src/Services/Actions/WishlistAction.js
@@ -6,3 +6,16 @@ export const ADD_TO_WISHLIST = '';
 export const GET_WISHLIST_PRODUCTS = '';
 export const REMOVE_FROM_WISHLIST = '';
 export const REMOVE_ALL_FROM_WISHLIST = '';
+
+// Add to wishlist
+export const addToWishlist = async (dispatch, product) => {
+  try {
+    const res = await axios.post(
+      'http://localhost:8080/api/addtowishlist',
+      product
+    );
+    dispatch({type: ADD_TO_WISHLIST, payload: res});
+  } catch (error) {
+    dispatch({type: ERROR, payload: error});
+  }
+};
