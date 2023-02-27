03bc4411bbb65a034533c3295c42ab8e56c0cf03
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-27 17:55:16 +0530

    User signup fuction added is user action

--- client/src/Services/Actions/UserAction.js
+++ client/src/Services/Actions/UserAction.js
@@ -4,4 +4,14 @@ import axios from 'axios';
 export const SIGNUP = 'SIGNUP';
 export const SIGNIN = 'SIGNIN';
 export const SIGNOUT = 'SIGNOUT';
-export const IS_ERROR = 'IS_ERROR'
+export const IS_ERROR = 'IS_ERROR';
+
+// Signup/Register
+export const signup = async (dispatch, user) => {
+  try {
+    const res = await axios.post(`http://localhost:8080/api/signup`, user);
+    dispatch({type: SIGNUP, payload: res});
+  } catch (error) {
+    dispatch({type: IS_ERROR, payload: error});
+  }
+};
