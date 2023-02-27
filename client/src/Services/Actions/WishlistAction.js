87351b2b3d6df39945aa7128833e2e407ca5caed
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-27 11:24:44 +0530

    WislistAction is created with all action types

---
+++ client/src/Services/Actions/WishlistAction.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+// Action types
+export const ERROR = '';
+export const ADD_TO_WISHLIST = '';
+export const GET_WISHLIST_PRODUCTS = '';
+export const REMOVE_FROM_WISHLIST = '';
+export const REMOVE_ALL_FROM_WISHLIST = '';
