4e3f90d1e4f55a8a545161fe05674f53ce018d1c
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-01 00:37:46 +0530

    Product details page ui structure is created

--- client/src/Pages/ProductDetailsPage/ProductDetailsPage.jsx
+++ client/src/Pages/ProductDetailsPage/ProductDetailsPage.jsx
@@ -1,16 +1,21 @@
-import React, { useEffect } from 'react'
-import styles from './ProductDetailsPage.module.css'
-import { useParams } from 'react-router-dom'
-import { useSelector,useDispatch } from 'react-redux'
+import React, {useEffect} from 'react';
+import axios from axios;
+import styles from './ProductDetailsPage.module.css';
+import {useParams} from 'react-router-dom';
+import {useSelector, useDispatch} from 'react-redux';
 
 const ProductDetailsPage = () => {
-    const {productid} = useParams();
-    const {products} = useSelector((state)=>state.ProductReducer)
-    const product = products.filter((elem)=>elem._id===productid)
-    console.log(product)
+  const {productid} = useParams();
+  const {products} = useSelector((state) => state.ProductReducer);
+  const product = products.filter((elem) => elem._id === productid);
+  console.log(product);
   return (
-    <div>ProductDetailsPage</div>
-  )
-}
+    <div className={styles.productDetailsContainer}>
+      <section className={styles.leftSection}>
+      </section>
+      <section className={styles.rightSection}></section>
+    </div>
+  );
+};
 
-export default ProductDetailsPage
+export default ProductDetailsPage;
