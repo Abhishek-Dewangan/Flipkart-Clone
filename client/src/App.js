5a5ce8deab84e8ae6a3cd7247ae5fb95f8be9087
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-28 23:40:22 +0530

    Product detail page is added in app.js as routes

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import {Routes, Route} from 'react-router-dom';
 import LandingPage from './Pages/LandingPage/LandingPage';
 import ProductCategoryPage from './Pages/ProductCategoryPage/ProductCategoryPage';
 import CartPage from './Pages/CartPage/CartPage';
+import ProductDetailsPage from './Pages/ProductDetaitsPage/ProductDetailsPage';
 
 function App() {
   return (
@@ -14,6 +15,10 @@ function App() {
         <Route path='/' element={<LandingPage />} />
         <Route path='/category/:category' element={<ProductCategoryPage />} />
         <Route path='/cart' element={<CartPage />} />
+        <Route
+          path='/productdetail/:productid'
+          element={<ProductDetailsPage />}
+        />
       </Routes>
       <Footer />
     </div>
