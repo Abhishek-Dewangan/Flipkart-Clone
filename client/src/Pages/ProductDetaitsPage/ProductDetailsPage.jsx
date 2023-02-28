40e219b8bd2c0ca4cc25b95d196edfbebfb40809
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-28 23:45:09 +0530

    Got the product id in product detail page with help of useParams hook

--- client/src/Pages/ProductDetaitsPage/ProductDetailsPage.jsx
+++ client/src/Pages/ProductDetaitsPage/ProductDetailsPage.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import styles from './ProductDetailsPage.module.css'
+import { useParams } from 'react-router-dom'
 
 const ProductDetailsPage = () => {
+    const {productid} = useParams();
   return (
     <div>ProductDetailsPage</div>
   )
