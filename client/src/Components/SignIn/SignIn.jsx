fc7e3228600c21693e3cebf7965bc7503f3a64be
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-01 12:19:44 +0530

    Login component is renamed as signin

---
+++ client/src/Components/SignIn/SignIn.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './SignIn.module.css';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const SignIn = ({show, handleClose, handleShow}) => {
+  return (
+    <Modal show={show} onHide={handleClose} className={styles.signinContainer}>
+      <Modal.Header closeButton>
+        <Modal.Title>Modal heading</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+      <Modal.Footer>
+        <Button variant='secondary' onClick={handleClose}>
+          Close
+        </Button>
+        <Button variant='secondary' onClick={handleClose}>
+          Save Changes
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default SignIn;
