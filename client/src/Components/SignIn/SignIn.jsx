81ff6ac8210d6ba56406ebd07f917b978be18658
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-02 00:33:48 +0530

    Server call for signin and signin form is created

--- client/src/Components/SignIn/SignIn.jsx
+++ client/src/Components/SignIn/SignIn.jsx
@@ -3,33 +3,72 @@ import styles from './SignIn.module.css';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import signinImage from '../../Assets/Images/signinimage.png';
+import {signin} from '../../Services/Actions/UserAction';
+import {useDispatch} from 'react-redux';
 
 const SignIn = ({showSignin, handleCloseSignin, handleShowSignup}) => {
+  const dispatch = useDispatch();
+  const submit = (e) => {
+    e.preventDefault();
+    const user = {
+      email: e.target.email.value,
+      password: e.target.password.value,
+    };
+    // console.log(user);
+    signin(dispatch, user);
+  };
   return (
-    <Modal
-      show={showSignin}
-      onHide={handleCloseSignin}
-      className={styles.signinContainer}
-    >
-      <Modal.Header closeButton>
-        <Modal.Title>SignIn</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>SignIn</Modal.Body>
-      <Modal.Footer>
-        <Button variant='secondary' onClick={handleCloseSignin}>
-          Close
-        </Button>
-      </Modal.Footer>
-      <button
-        className={styles.signin}
-        onClick={() => {
-          handleShowSignup();
-          handleCloseSignin();
-        }}
+    <div className={styles.signinContainer}>
+      <Modal
+        size=''
+        show={showSignin}
+        onHide={handleCloseSignin}
+        className={styles.modal}
+        closebutton
       >
-        New to Flipkart? Create an account
-      </button>
-    </Modal>
+        <div className={styles.signinBody}>
+          <div className={styles.imageDiv}>
+            <img src={signinImage} alt='' className={styles.signinSideImage} />
+          </div>
+          <div className={styles.formDiv}>
+            <Modal.Header closeButton={true}>
+              <Modal.Title className={styles.formTitle}>SignIn</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <form onSubmit={submit} className={styles.signinForm}>
+                <input
+                  required
+                  type={'email'}
+                  placeholder='Email'
+                  name='email'
+                />{' '}
+                <br />
+                <input
+                  required
+                  type={'password'}
+                  placeholder='Password'
+                  name='password'
+                />{' '}
+                <br />
+                <input type={'submit'} value='Submit' /> <br />
+              </form>
+            </Modal.Body>
+            <Modal.Footer>
+              <button
+                className={styles.signinBtn}
+                onClick={() => {
+                  handleShowSignup();
+                  handleCloseSignin();
+                }}
+              >
+                Already have an account? SignIn
+              </button>
+            </Modal.Footer>
+          </div>
+        </div>
+      </Modal>
+    </div>
   );
 };
 
