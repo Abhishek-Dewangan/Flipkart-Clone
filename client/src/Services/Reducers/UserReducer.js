b8cd445884382e9f2b4f4405357732669d806e70
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-02 00:22:03 +0530

    Is error and is loading status is added in every action type case

--- client/src/Services/Reducers/UserReducer.js
+++ client/src/Services/Reducers/UserReducer.js
@@ -1,31 +1,63 @@
-import {IS_ERROR, SIGNIN, SIGNOUT, SIGNUP} from '../Actions/UserAction';
+import {
+  IS_ERROR,
+  IS_LOADING,
+  SIGNIN,
+  SIGNOUT,
+  SIGNUP,
+} from '../Actions/UserAction';
 
 const initialState = {
-  token: '',
   isError: false,
   isLoading: false,
+  token: '',
   user: {},
+  message: '',
 };
 
 export const UserReducer = (state = initialState, action) => {
   switch (action.type) {
+    case IS_LOADING: {
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+        message: '',
+      };
+    }
+    case IS_ERROR: {
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+        message: action.message,
+      };
+    }
     case SIGNIN: {
       return {
         ...state,
+        isLoading: false,
+        isError: false,
         token: action.payload.token,
         user: action.payload,
+        message: action.payload.message,
       };
     }
     case SIGNUP: {
       return {
         ...state,
+        isLoading: false,
+        isError: false,
+        message: action.payload.message,
       };
     }
     case SIGNOUT: {
       return {
         ...state,
+        isLoading: false,
+        isError: false,
         token: '',
         user: {},
+        message: action.payload.message,
       };
     }
     default: {
