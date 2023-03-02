4e135388fa101dfafe2d0070c925fc69b0955284
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-03 00:31:56 +0530

    User reducer is imported in store.js and compbined with other reducers

--- client/src/Services/store.js
+++ client/src/Services/store.js
@@ -1,6 +1,7 @@
 import {legacy_createStore as createStore, combineReducers} from 'redux';
 import {ProductReducer} from './Reducers/ProductReducer';
+import {UserReducer} from './Reducers/UserReducer';
 
-const rootReducer = combineReducers({ProductReducer});
+const rootReducer = combineReducers({ProductReducer, UserReducer});
 
 export const store = createStore(rootReducer);
