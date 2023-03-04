5059466db5fa01c62724496b63289837fa9f3497
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-04 23:57:24 +0530

    Added wishlist reducer in redux store

--- client/src/Services/store.js
+++ client/src/Services/store.js
@@ -1,7 +1,12 @@
 import {legacy_createStore as createStore, combineReducers} from 'redux';
 import {ProductReducer} from './Reducers/ProductReducer';
 import {UserReducer} from './Reducers/UserReducer';
+import {WishlistReducer} from './Reducers/WishlistReducer';
 
-const rootReducer = combineReducers({ProductReducer, UserReducer});
+const rootReducer = combineReducers({
+  ProductReducer,
+  UserReducer,
+  WishlistReducer,
+});
 
 export const store = createStore(rootReducer);
