fe35098d39bac314400c10d9523305fa1970cb06
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-05 02:26:06 +0530

    Update initial state in cart reducer with isError, isLoading and isSuccess status

--- client/src/Services/Reducers/cartReducer.js
+++ client/src/Services/Reducers/cartReducer.js
@@ -1,40 +1,73 @@
 import {
+  IS_ERROR,
+  IS_LOADING,
   ADD_TO_CART,
   GET_CART_DATA,
-  ERROR,
   REMOVE_ALL_FROM_CART,
   REMOVE_FROM_CART,
 } from '../Actions/CartAction';
 
 const initialState = {
-  cartData: [],
-  isLoading: false,
   isError: false,
+  isLoading: false,
+  isSuccess: false,
+  cartData: [],
 };
 
 export const CartReducer = (state = initialState, action) => {
   switch (action.type) {
+    case IS_LOADING: {
+      return {
+        ...state,
+        isError: false,
+        isLoading: true,
+        isSuccess: false,
+      };
+    }
+    case IS_ERROR: {
+      return {
+        ...state,
+        isError: true,
+        isLoading: false,
+        isSuccess: false,
+      };
+    }
     case ADD_TO_CART: {
       return {
         ...state,
-        cartData: [...state.cartData, action.payload],
+        isError: false,
+        isLoading: false,
+        isSuccess: true,
+        cartData: [...state.cartData, action.payload.data],
       };
     }
     case GET_CART_DATA: {
       return {
         ...state,
-        cartData: action.payload,
+        isError: false,
+        isLoading: false,
+        isSuccess: true,
+        cartData: action.payload.data,
       };
     }
     case REMOVE_FROM_CART: {
+      const updatedCartData = state.cartData.filter(
+        (elem) => elem._id !== action.payload.data._id
+      );
       return {
         ...state,
-        cartData: action.payload,
+        isError: false,
+        isLoading: false,
+        isSuccess: true,
+        cartData: updatedCartData,
       };
     }
     case REMOVE_ALL_FROM_CART: {
       return {
         ...state,
+        isError: false,
+        isLoading: false,
+        isSuccess: true,
         cartData: [],
       };
     }
