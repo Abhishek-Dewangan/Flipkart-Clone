778d083999c5a78760f51385e06c1f690093e971
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-04 23:58:41 +0530

    Added Is error and is loading type action and update addToWishlist fuction

--- client/src/Services/Actions/WishlistAction.js
+++ client/src/Services/Actions/WishlistAction.js
@@ -1,22 +1,26 @@
 import axios from 'axios';
 
 // Action types
-export const ERROR = '';
-export const ADD_TO_WISHLIST = '';
-export const GET_WISHLIST_PRODUCTS = '';
-export const REMOVE_FROM_WISHLIST = '';
-export const REMOVE_ALL_FROM_WISHLIST = '';
+export const IS_ERROR = 'IS_ERROR';
+export const IS_LOADING = 'IS_LOADING';
+export const ADD_TO_WISHLIST = 'ADD_TO_WISHLIST';
+export const GET_WISHLIST_PRODUCTS = 'GET_WISHLIST_PRODUCTS';
+export const REMOVE_FROM_WISHLIST = 'REMOVE_FROM_WISHLIST';
+export const REMOVE_ALL_FROM_WISHLIST = 'REMOVE_ALL_FROM_WISHLIST';
 
 // Add to wishlist
 export const addToWishlist = async (dispatch, product) => {
+  dispatch({type: IS_LOADING});
   try {
     const res = await axios.post(
       'http://localhost:8080/api/addtowishlist',
       product
     );
-    dispatch({type: ADD_TO_WISHLIST, payload: res});
+    console.log(res.data);
+    dispatch({type: ADD_TO_WISHLIST, payload: res.data});
   } catch (error) {
-    dispatch({type: ERROR, payload: error});
+    console.log(error);
+    dispatch({type: IS_ERROR, payload: error.response.data});
   }
 };
 
@@ -28,7 +32,7 @@ export const getWishlistProducts = async (dispatch, userId) => {
     );
     dispatch({type: GET_WISHLIST_PRODUCTS, payload: wishlistData});
   } catch (error) {
-    dispatch({type: ERROR, payload: error});
+    dispatch({type: IS_ERROR, payload: error});
   }
 };
 
@@ -40,7 +44,7 @@ export const removeFromWishlist = async (dispatch, productId) => {
     );
     dispatch({type: REMOVE_FROM_WISHLIST, payload: res});
   } catch (error) {
-    dispatch({type: ERROR, payload: error});
+    dispatch({type: IS_ERROR, payload: error});
   }
 };
 
@@ -50,6 +54,6 @@ export const removeAllFromWishlist = async (dispatch, userId) => {
     const res = await axios.delete(`http://localhost:8080/api/${userId}`);
     dispatch({type: REMOVE_ALL_FROM_WISHLIST, payload: res});
   } catch (error) {
-    dispatch({type: ERROR, payload: error});
+    dispatch({type: IS_ERROR, payload: error});
   }
 };
