6ef6c9165b9b259d2f65b92db0e2eeaf9d37e344
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-05 19:21:25 +0530

    CartReducer is combined with root reducer

--- client/src/Services/store.js
+++ client/src/Services/store.js
@@ -2,11 +2,13 @@ import {legacy_createStore as createStore, combineReducers} from 'redux';
 import {ProductReducer} from './Reducers/ProductReducer';
 import {UserReducer} from './Reducers/UserReducer';
 import {WishlistReducer} from './Reducers/WishlistReducer';
+import {CartReducer} from './Reducers/CartReducer';
 
 const rootReducer = combineReducers({
   ProductReducer,
   UserReducer,
   WishlistReducer,
+  CartReducer,
 });
 
 export const store = createStore(rootReducer);
