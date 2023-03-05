505ef099909d1db1cb9f59546a89661a42d0dcce
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-05 20:27:39 +0530

    Minor update in all user action fuctions where cheking the response

--- client/src/Services/Actions/UserAction.js
+++ client/src/Services/Actions/UserAction.js
@@ -12,10 +12,10 @@ export const signup = async (dispatch, user) => {
   dispatch({type: IS_LOADING});
   try {
     const res = await axios.post(`http://localhost:8080/api/signup`, user);
-    console.log(res.data);
+    // console.log(res.data);
     dispatch({type: SIGNUP, payload: res.data});
   } catch (error) {
-    console.log(error.response.data);
+    // console.log(error.response.data);
     dispatch({type: IS_ERROR, payload: error.response.data});
   }
 };
@@ -25,11 +25,11 @@ export const signin = async (dispatch, user) => {
   dispatch({type: IS_LOADING});
   try {
     const res = await axios.post(`http://localhost:8080/api/signin`, user);
-    console.log(res.data);
+    // console.log(res.data);
     localStorage.setItem('user', JSON.stringify(res.data.user));
     dispatch({type: SIGNIN, payload: res.data});
   } catch (error) {
-    console.log(error.response.data);
+    // console.log(error.response.data);
     dispatch({type: IS_ERROR, payload: error.response.data});
   }
 };
@@ -41,10 +41,10 @@ export const signout = async (dispatch, token) => {
   try {
     const res = await axios.post(`http://localhost:8080/api/signout`, {token:token});
     localStorage.removeItem('user');
-    console.log(res.data);
+    // console.log(res.data);
     dispatch({type: SIGNOUT, payload: res.data});
   } catch (error) {
-    console.log(error);
+    // console.log(error.response.data);
     dispatch({type: IS_ERROR, payload: error.response.data});
   }
 };
