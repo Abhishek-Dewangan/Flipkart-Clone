67fc83dea177381a7b4400af7fbce6e3aa66d459
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-07 01:08:07 +0530

    Wishlist page left section is created with proper styles

--- client/src/Pages/WishlistPage/WishlistPage.jsx
+++ client/src/Pages/WishlistPage/WishlistPage.jsx
@@ -3,6 +3,10 @@ import styles from './WishlistPage.module.css';
 import {useDispatch, useSelector} from 'react-redux';
 import {Link} from 'react-router-dom';
 import {signout} from '../../Services/Actions/UserAction';
+import {CgProfile} from 'react-icons/cg';
+import {BsFillArrowUpCircleFill} from 'react-icons/bs';
+import {FaShoppingCart} from 'react-icons/fa';
+import {BiLogOutCircle} from 'react-icons/bi';
 
 const WishlistPage = () => {
   const dispatch = useDispatch();
@@ -10,23 +14,35 @@ const WishlistPage = () => {
   const wishlist = useSelector((state) => state.WishlistReducer);
   return (
     <div className={styles.wishlistContainer}>
-      <section>
-        <div>
-          <img src='' alt='' />
-          <div>
-            <p>Hello,</p>
+      <section className={styles.leftSection}>
+        <div className={styles.profile}>
+          <CgProfile className={styles.profileIcon} size={50} />
+          <div className={styles.profileDetails}>
+            <p className={styles.greet}>Hello,</p>
             <p>{user.first_name + ' ' + user.last_name}</p>
           </div>
         </div>
-        <div>
-          <Link to={'/myorders'}>MY ORDERS</Link>
+        <div className={styles.otherLinks}>
+          <div>
+            <Link className={styles.link} to={'/myorders'}>
+              <BsFillArrowUpCircleFill className={styles.orderIcon} /> MY ORDERS
+            </Link>
+          </div>
           <hr />
-          <Link to={'/cart'}>MY CART</Link>
+          <div>
+            <Link className={styles.link} to={'/cart'}>
+              <FaShoppingCart className={styles.cartIcon} /> MY CART
+            </Link>
+          </div>
           <hr />
-          <p onClick={() => signout(dispatch, user.token)}>LOGOUT</p>
+          <div>
+            <Link className={styles.link} onClick={() => signout(dispatch, user.token)}>
+              <BiLogOutCircle className={styles.logoutIcon} /> LOGOUT
+            </Link>
+          </div>
         </div>
       </section>
-      <section></section>
+      <section className={styles.rightSection}></section>
     </div>
   );
 };
