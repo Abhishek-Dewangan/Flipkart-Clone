c5d2ddc8a133156b90ece5202079ff396762c679
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-06 11:23:09 +0530

    MyOrder page is created with style file

---
+++ client/src/Pages/MyOrdersPage/MyOrdersPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './MyOrdersPage.module.css'
+
+const MyOrdersPage = () => {
+  return (
+    <div className={styles.myOrderContainer}>
+      <h1>MyOrdersPage</h1>
+    </div>
+  );
+};
+
+export default MyOrdersPage;
