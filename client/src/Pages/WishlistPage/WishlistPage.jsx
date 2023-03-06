40d6b516071b1a0904c2e2aa5a76a8013a0b1011
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-06 23:46:52 +0530

    Wishlist left section top part is implemented

--- client/src/Pages/WishlistPage/WishlistPage.jsx
+++ client/src/Pages/WishlistPage/WishlistPage.jsx
@@ -3,8 +3,20 @@ import styles from './WishlistPage.module.css';
 
 const WishlistPage = () => {
   return (
-    <div className={styles.wishlistContainer}><h1>WishlistPage</h1></div>
-  )
-}
+    <div className={styles.wishlistContainer}>
+      <section>
+        <div>
+          <img src='' alt='' />
+          <div>
+            <p>Hello,</p>
+            <p>Abhishek dewanga</p>
+          </div>
+        </div>
+        <div></div>
+      </section>
+      <section></section>
+    </div>
+  );
+};
 
-export default WishlistPage
+export default WishlistPage;
