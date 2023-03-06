78c6971ac3eca943dae12f9ac0be0efcfe1395b8
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-06 11:20:00 +0530

    Wishlist and myorders page is added in routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
 // import './App.css';
+import {useEffect, useState} from 'react';
+import {Routes, Route} from 'react-router-dom';
+import {getPrducts} from './Services/Actions/ProductAction';
+import {useDispatch, useSelector} from 'react-redux';
+import {getWishlistProducts} from './Services/Actions/WishlistAction';
+import {getCartProducts} from './Services/Actions/CartAction';
 import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
-import {Routes, Route} from 'react-router-dom';
+import SignIn from './Components/SignIn/SignIn';
+import SignUp from './Components/SignUp/SignUp';
 import LandingPage from './Pages/LandingPage/LandingPage';
 import ProductCategoryPage from './Pages/ProductCategoryPage/ProductCategoryPage';
 import CartPage from './Pages/CartPage/CartPage';
+import WishlistPage from './Pages/WishlistPage/WishlistPage';
 import ProductDetailsPage from './Pages/ProductDetailsPage/ProductDetailsPage';
-import {useEffect, useState} from 'react';
-import {getPrducts} from './Services/Actions/ProductAction';
-import {useDispatch, useSelector} from 'react-redux';
-import SignIn from './Components/SignIn/SignIn';
-import SignUp from './Components/SignUp/SignUp';
-import {getWishlistProducts} from './Services/Actions/WishlistAction';
-import {getCartProducts} from './Services/Actions/CartAction';
+import MyOrdersPage from './Pages/MyOrdersPage/MyOrdersPage';
 
 function App() {
   const dispatch = useDispatch();
@@ -25,11 +27,17 @@ function App() {
   const handleCloseSignup = () => setShowSignup(false);
   const handleShowSignup = () => setShowSignup(true);
 
+  // Calling getProduct fuction to store all product in redux store on first render
   useEffect(() => {
     getPrducts(dispatch);
-    getWishlistProducts(dispatch, user.userId);
-    getCartProducts(dispatch, user.userId);
   }, []);
+
+  // On user changing calling getWishlist and getCart product function
+  useEffect(() => {
+    user.userId && getWishlistProducts(dispatch, user.userId);
+    user.userId && getCartProducts(dispatch, user.userId);
+  }, [user]);
+
   return (
     <div className='App'>
       <Header handleShowSignin={handleShowSignin} />
@@ -47,6 +55,8 @@ function App() {
         <Route path='/' element={<LandingPage />} />
         <Route path='/category/:category' element={<ProductCategoryPage />} />
         <Route path='/cart' element={<CartPage />} />
+        <Route path='/wishlist' element={<WishlistPage />} />
+        <Route path='/myorders' element={<MyOrdersPage />} />
         <Route
           path='/productdetail/:productid'
           element={<ProductDetailsPage />}
