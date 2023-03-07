0b120b7e632db65ae06a495944721467ab7262fd
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-07 20:14:21 +0530

    Minimum heigh of product image is updated as 100px

--- client/src/Pages/WishlistPage/WishlistPage.jsx
+++ client/src/Pages/WishlistPage/WishlistPage.jsx
@@ -17,6 +17,8 @@ const WishlistPage = () => {
   const {user} = useSelector((state) => state.UserReducer);
   const wishlist = useSelector((state) => state.WishlistReducer);
 
+  const alertMessage = () => {}
+
   return (
     <div className={styles.wishlistContainer}>
       <section className={styles.leftSection}>
