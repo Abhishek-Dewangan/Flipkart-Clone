16b521814b26cac94bc256062fe8e0fd2995148e
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-07 19:44:57 +0530

    Successfully showed github data with proper styles

--- client/src/Pages/WishlistPage/WishlistPage.jsx
+++ client/src/Pages/WishlistPage/WishlistPage.jsx
@@ -3,15 +3,20 @@ import styles from './WishlistPage.module.css';
 import {useDispatch, useSelector} from 'react-redux';
 import {Link} from 'react-router-dom';
 import {signout} from '../../Services/Actions/UserAction';
+import fAssured from '../../Assets/Images/f-assured.png';
 import {CgProfile} from 'react-icons/cg';
 import {BsFillArrowUpCircleFill} from 'react-icons/bs';
 import {FaShoppingCart} from 'react-icons/fa';
 import {BiLogOutCircle} from 'react-icons/bi';
+import {GrNext} from 'react-icons/gr';
+import {MdDelete} from 'react-icons/md';
+import {removeFromWishlist} from '../../Services/Actions/WishlistAction';
 
 const WishlistPage = () => {
   const dispatch = useDispatch();
   const {user} = useSelector((state) => state.UserReducer);
   const wishlist = useSelector((state) => state.WishlistReducer);
+
   return (
     <div className={styles.wishlistContainer}>
       <section className={styles.leftSection}>
@@ -23,19 +28,21 @@ const WishlistPage = () => {
           </div>
         </div>
         <div className={styles.otherLinks}>
-          <div>
+          <div className={styles.linksDiv}>
             <Link className={styles.link} to={'/myorders'}>
-              <BsFillArrowUpCircleFill className={styles.orderIcon} /> MY ORDERS
+              <BsFillArrowUpCircleFill className={styles.orderIcon} /> MY ORDERS{' '}
+              <GrNext className={styles.nextIcon} />
             </Link>
           </div>
           <hr />
-          <div>
+          <div className={styles.linksDiv}>
             <Link className={styles.link} to={'/cart'}>
-              <FaShoppingCart className={styles.cartIcon} /> MY CART
+              <FaShoppingCart className={styles.cartIcon} /> MY CART{' '}
+              <GrNext className={styles.nextIcon} />
             </Link>
           </div>
           <hr />
-          <div>
+          <div className={styles.linksDiv}>
             <Link
               className={styles.link}
               onClick={() => signout(dispatch, user.token)}
@@ -46,7 +53,56 @@ const WishlistPage = () => {
         </div>
       </section>
       <section className={styles.rightSection}>
-        <p>My Wishlist ({wishlist.wishlistData.length})</p>
+        <h5 className={styles.heading}>
+          My Wishlist ({wishlist.wishlistData.length})
+        </h5>
+        <hr />
+        {wishlist.wishlistData.length ? (
+          wishlist.wishlistData.map((elem) => {
+            // Calculating discount percent of products
+            const discount = Math.floor(
+              100 - (elem.current_price * 100) / elem.original_price
+            );
+            return (
+              <div key={elem._id}>
+                <div className={styles.productBox}>
+                  <div className={styles.imgDiv}>
+                    <img
+                      src={elem.thumbnail}
+                      alt={elem.name}
+                      className={styles.productImage}
+                    />
+                  </div>
+                  <div className={styles.productDetails}>
+                    <span className={styles.productNmae}>{elem.name}</span>
+                    <img
+                      className={styles.fAssured}
+                      src={fAssured}
+                      alt='Flipkart assured'
+                    />
+                    <p className={styles.price}>
+                      <span className={styles.currentPrice}>
+                        ₹{elem.current_price}
+                      </span>{' '}
+                      <strike className={styles.originalPrice}>
+                        ₹{elem.original_price}
+                      </strike>
+                      <span className={styles.discount}>{discount}% off</span>
+                    </p>
+                  </div>
+                  <MdDelete
+                    title='Remove'
+                    className={styles.deleteIcon}
+                    onClick={() => removeFromWishlist(dispatch, elem._id)}
+                  />
+                </div>
+                <hr />
+              </div>
+            );
+          })
+        ) : (
+          <h1>Your wishlist data is empty</h1>
+        )}
       </section>
     </div>
   );
