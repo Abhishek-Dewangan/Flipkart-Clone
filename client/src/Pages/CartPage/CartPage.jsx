79fdf43cf33eff2fbd9adfbaf0119ead569376b8
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-09 23:47:15 +0530

    Redered cart product with remove and buy this now button

--- client/src/Pages/CartPage/CartPage.jsx
+++ client/src/Pages/CartPage/CartPage.jsx
@@ -1,10 +1,69 @@
 import React from 'react';
 import styles from './CartPage.module.css';
+import {useSelector} from 'react-redux';
+import fAssured from '../../Assets/Images/f-assured.png';
+import {Link} from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 
 const CartPage = () => {
+  const cart = useSelector((state) => state.CartReducer);
+
   return (
     <div className={styles.cartPageContainer}>
-      <h1>cartpage</h1>
+      <section className={styles.leftSection}>
+        <h1>cartpage</h1>
+        {cart.cartData.length &&
+          cart.cartData.map((elem) => {
+            // Calculating discount percent of products
+            const discount = Math.floor(
+              100 - (elem.current_price * 100) / elem.original_price
+            );
+            return (
+              <div key={elem._id}>
+                <div className={styles.productBox}>
+                  <Link
+                    to={`/productdetail/:${elem.productId}`}
+                    className={styles.imgDiv}
+                  >
+                    <img
+                      src={elem.thumbnail}
+                      alt={elem.name}
+                      className={styles.productImage}
+                    />
+                  </Link>
+                  <div className={styles.productDetails}>
+                    <Link
+                      to={`/productdetail/:${elem.productId}`}
+                      className={styles.productName}
+                    >
+                      <span>{elem.name}</span>
+                    </Link>
+                    <img
+                      className={styles.fAssured}
+                      src={fAssured}
+                      alt='Flipkart assured'
+                    />
+                    <p className={styles.price}>
+                      <span className={styles.currentPrice}>
+                        ₹{elem.current_price}
+                      </span>{' '}
+                      <strike className={styles.originalPrice}>
+                        ₹{elem.original_price}
+                      </strike>
+                      <span className={styles.discount}>{discount}% off</span>
+                    </p>
+                    <div className={styles.btnDiv}>
+                      <Button variant='outline-danger'>Remove</Button>
+                      <Button variant='outline-warning'>Buy this now</Button>
+                    </div>
+                  </div>
+                </div>
+                <hr />
+              </div>
+            );
+          })}
+      </section>
+      <section className={styles.rightSection}>Right Section</section>
     </div>
   );
 };
