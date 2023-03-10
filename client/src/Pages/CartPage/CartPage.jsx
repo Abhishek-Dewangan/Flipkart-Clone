ecab582da028ff4a2c7e5e11acce9d15c05ca3f2
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-10 13:00:09 +0530

    Added empty image, it will be visible in case of empty cart

--- client/src/Pages/CartPage/CartPage.jsx
+++ client/src/Pages/CartPage/CartPage.jsx
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import styles from './CartPage.module.css';
 import {useSelector, useDispatch} from 'react-redux';
 import fAssured from '../../Assets/Images/f-assured.png';
+import emptyImage from '../../Assets/Images/empty.png';
 import {Link, useNavigate} from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import {removeFromCart} from '../../Services/Actions/CartAction';
@@ -15,68 +16,88 @@ const CartPage = () => {
   return (
     <div className={styles.cartPageContainer}>
       <section className={styles.leftSection}>
-        {cart.cartData.length &&
-          cart.cartData.map((elem) => {
-            // Calculating discount percent of products
-            const discount = Math.floor(
-              100 - (elem.current_price * 100) / elem.original_price
-            );
-            return (
-              <div key={elem._id}>
-                <div className={styles.productBox}>
-                  <Link
-                    to={`/productdetail/:${elem.productId}`}
-                    className={styles.imgDiv}
-                  >
-                    <img
-                      src={elem.thumbnail}
-                      alt={elem.name}
-                      className={styles.productImage}
-                    />
-                  </Link>
-                  <div className={styles.productDetails}>
+        {cart.cartData.length ? (
+          <div>
+            <h5 className={styles.leftSecHeading}>
+              My Cart({cart.cartData.length})
+            </h5>
+            <hr />
+          </div>
+        ) : (
+          ''
+        )}
+        <div className={styles.cartData}>
+          {cart.cartData.length ? (
+            cart.cartData.map((elem) => {
+              // Calculating discount percent of products
+              const discount = Math.floor(
+                100 - (elem.current_price * 100) / elem.original_price
+              );
+              return (
+                <div key={elem._id}>
+                  <div className={styles.productBox}>
                     <Link
                       to={`/productdetail/:${elem.productId}`}
-                      className={styles.productName}
+                      className={styles.imgDiv}
                     >
-                      <span>{elem.name}</span>
+                      <img
+                        src={elem.thumbnail}
+                        alt={elem.name}
+                        className={styles.productImage}
+                      />
                     </Link>
-                    <img
-                      className={styles.fAssured}
-                      src={fAssured}
-                      alt='Flipkart assured'
-                    />
-                    <p className={styles.price}>
-                      <span className={styles.currentPrice}>
-                        ₹{elem.current_price}
-                      </span>{' '}
-                      <strike className={styles.originalPrice}>
-                        ₹{elem.original_price}
-                      </strike>
-                      <span className={styles.discount}>{discount}% off</span>
-                    </p>
-                    <div className={styles.btnDiv}>
-                      <Button
-                        variant='outline-danger'
-                        onClick={() => removeFromCart(dispatch, elem._id)}
+                    <div className={styles.productDetails}>
+                      <Link
+                        to={`/productdetail/:${elem.productId}`}
+                        className={styles.productName}
                       >
-                        Remove
-                      </Button>
-                      <Button
-                        variant='outline-warning'
-                        onClick={() =>
-                          navigate(`'productdetail/:${elem.productId}`)
-                        }
-                      >
-                        Buy this now
-                      </Button>
+                        <span>{elem.name}</span>
+                      </Link>
+                      <img
+                        className={styles.fAssured}
+                        src={fAssured}
+                        alt='Flipkart assured'
+                      />
+                      <p className={styles.price}>
+                        <span className={styles.currentPrice}>
+                          ₹{elem.current_price}
+                        </span>{' '}
+                        <strike className={styles.originalPrice}>
+                          ₹{elem.original_price}
+                        </strike>
+                        <span className={styles.discount}>{discount}% off</span>
+                      </p>
+                      <div className={styles.btnDiv}>
+                        <Button
+                          variant='outline-danger'
+                          onClick={() => removeFromCart(dispatch, elem._id)}
+                        >
+                          Remove
+                        </Button>
+                        <Button
+                          variant='outline-warning'
+                          onClick={() =>
+                            navigate(`'productdetail/:${elem.productId}`)
+                          }
+                        >
+                          Buy this now
+                        </Button>
+                      </div>
                     </div>
                   </div>
+                  <hr />
                 </div>
-                <hr />
-              </div>
-            );
-          })}
+              );
+            })
+          ) : (
+            <div className={styles.emptyCart}>
+              <img src={emptyImage} alt='Empty Item' />
+              <h4>Your cart is empty!</h4>
+              <p>Add items to it now!</p>
+              <Button onClick={() => navigate('/')}>Shop now</Button>
+            </div>
+          )}
+        </div>
       </section>
       <section className={styles.rightSection}>
         <CartSidebar />
