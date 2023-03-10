b29b0431ac5cb21536582253c67e450f00f7bd00
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-10 13:39:12 +0530

    Added slider side heading in Proudct slider

--- client/src/Components/ProductSlider/ProductSlider.jsx
+++ client/src/Components/ProductSlider/ProductSlider.jsx
@@ -6,48 +6,53 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
-const ProductSlider = ({products}) => {
+const ProductSlider = ({products, heading}) => {
   return (
-    <Swiper
-      className={styles.swiper}
-      modules={[Navigation]}
-      slidesPerView={1}
-      spaceBetween={10}
-      navigation={{clickable: true}}
-      breakpoints={{
-        '@0.00': {
-          slidesPerView: 1,
-          // spaceBetween: 10,
-        },
-        '@0.75': {
-          slidesPerView: 2,
-          // spaceBetween: 20,
-        },
-        '@1.00': {
-          slidesPerView: 3,
-          // spaceBetween: 40,
-        },
-        '@1.50': {
-          slidesPerView: 4,
-          // spaceBetween: 50,
-        },
-      }}
-    >
-      {products.length &&
-        products.map((elem) => {
-          return (
-            <SwiperSlide key={elem._id} className={styles.swiperSlider}>
-              <img
-                className={styles.productImage}
-                src={elem.thumbnail}
-                alt=''
-              />
-              <p className={styles.productName}>{elem.name}</p>
-              <p>Under ₹{elem.current_price}</p>
-            </SwiperSlide>
-          );
-        })}
-    </Swiper>
+    <div className={styles.sliderDiv}>
+      <div className={styles.productHeading}>
+        <h2>{heading}</h2>
+      </div>
+      <Swiper
+        className={styles.swiper}
+        modules={[Navigation]}
+        slidesPerView={1}
+        spaceBetween={10}
+        navigation={{clickable: true}}
+        breakpoints={{
+          '@0.00': {
+            slidesPerView: 1,
+            // spaceBetween: 10,
+          },
+          '@0.75': {
+            slidesPerView: 2,
+            // spaceBetween: 20,
+          },
+          '@1.00': {
+            slidesPerView: 3,
+            // spaceBetween: 40,
+          },
+          '@1.50': {
+            slidesPerView: 4,
+            // spaceBetween: 50,
+          },
+        }}
+      >
+        {products.length &&
+          products.map((elem) => {
+            return (
+              <SwiperSlide key={elem._id} className={styles.swiperSlider}>
+                <img
+                  className={styles.productImage}
+                  src={elem.thumbnail}
+                  alt=''
+                />
+                <p className={styles.productName}>{elem.name}</p>
+                <p>Under ₹{elem.current_price}</p>
+              </SwiperSlide>
+            );
+          })}
+      </Swiper>
+    </div>
   );
 };
 
