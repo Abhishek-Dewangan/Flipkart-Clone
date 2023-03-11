528c70b7c43d5784b15bfe0a8abb54e9bbe321ab
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-12 00:41:13 +0530

    Added empty image whilse highlight and specification will be empty

--- client/src/Pages/ProductDetailsPage/ProductDetailsPage.jsx
+++ client/src/Pages/ProductDetailsPage/ProductDetailsPage.jsx
@@ -7,6 +7,7 @@ import {FaShoppingCart} from 'react-icons/fa';
 import {GiElectric} from 'react-icons/gi';
 import {AiFillStar} from 'react-icons/ai';
 import fAssured from '../../Assets/Images/f-assured.png';
+import emptyImage from '../../Assets/Images/empty.png';
 
 const ProductDetailsPage = () => {
   const dispatch = useDispatch();
@@ -20,93 +21,116 @@ const ProductDetailsPage = () => {
   }, [products]);
 
   useEffect(() => {
-    console.log(productDetails);
+    productDetails.name && console.log(productDetails);
   }, [productDetails]);
 
   return (
-    <div className={styles.productDetailsContainer}>
-      <section className={styles.leftSection}>
-        <div className={styles.productImageDiv}>
-          <img
-            className={styles.productImage}
-            src={productDetails.thumbnails}
-            alt={productDetails.name}
-          />
-        </div>
-        <div className={styles.buttonDiv}>
-          <button>
-            <FaShoppingCart /> ADD TO CART
-          </button>
-          <button>
-            <GiElectric /> BUY NOW
-          </button>
-        </div>
-      </section>
-      <section className={styles.rightSection}>
-        <div className={styles.highlightsBox}>
-          <p className={styles.productName}>{productDetails.name}</p>
-          <div className={styles.ratingDiv}>
-            <p className={styles.rating}>
-              {productDetails.rating} <AiFillStar className={styles.starIcon} />
-            </p>
-            {productDetails.f_assured ? (
-              <img className={styles.fAssured} src={fAssured} alt='F-Assured' />
-            ) : (
-              ''
-            )}
+    productDetails.name && (
+      <div className={styles.productDetailsContainer}>
+        <section className={styles.leftSection}>
+          <div className={styles.productImageDiv}>
+            <img
+              className={styles.productImage}
+              src={productDetails.thumbnails}
+              alt={productDetails.name}
+            />
           </div>
-          <span className={styles.specialPrice}>Special price</span>
-          <p className={styles.price}>
-            <span className={styles.currentPrice}>
-              ₹{productDetails.current_price}
-            </span>{' '}
-            <strike className={styles.originalPrice}>
-              ₹{productDetails.original_price}
-            </strike>
-            <span className={styles.discount}>
-              {productDetails.discount_percent}% off
-            </span>
-          </p>
-          <h5>Highlights</h5>
-          <ul>
-            {productDetails.highlights &&
-              productDetails.highlights.map((elem, i) => {
-                return <li key={i}>{elem}</li>;
-              })}
-          </ul>
-        </div>
-        {productDetails.specs ? (
-          <div className={styles.specificationBox}>
-            <h4>Specification</h4>
-            <hr />
-            {productDetails.specs.map((element, i) => {
-              return element.details ? (
-                <div key={i}>
-                  <h5>{element.title}</h5>
-                  {element.details.map((elem, index) => {
-                    return (
-                      <div key={index} className={styles.property}>
-                        <div>
-                          <span>{elem.property}</span>
-                        </div>
-                        <div>
-                          <span>{elem.value}</span>
-                        </div>
-                      </div>
-                    );
-                  })}
-                  <hr />
-                </div>
+          <div className={styles.buttonDiv}>
+            <button>
+              <FaShoppingCart /> ADD TO CART
+            </button>
+            <button>
+              <GiElectric /> BUY NOW
+            </button>
+          </div>
+        </section>
+        <section className={styles.rightSection}>
+          <div className={styles.highlightsBox}>
+            <p className={styles.productName}>{productDetails.name}</p>
+            <div className={styles.ratingDiv}>
+              <p className={styles.rating}>
+                {productDetails.rating}{' '}
+                <AiFillStar className={styles.starIcon} />
+              </p>
+              {productDetails.f_assured ? (
+                <img
+                  className={styles.fAssured}
+                  src={fAssured}
+                  alt='F-Assured'
+                />
               ) : (
                 ''
-              );
-            })}
+              )}
+            </div>
+            <span className={styles.specialPrice}>Special price</span>
+            <p className={styles.price}>
+              <span className={styles.currentPrice}>
+                ₹{productDetails.current_price}
+              </span>{' '}
+              <strike className={styles.originalPrice}>
+                ₹{productDetails.original_price}
+              </strike>
+              <span className={styles.discount}>
+                {productDetails.discount_percent}% off
+              </span>
+            </p>
+            <h5>Highlights</h5>
+            <hr />
+            <ul>
+              {productDetails.highlights.length ? (
+                productDetails.highlights.map((elem, i) => {
+                  return (
+                    <li key={i} className={styles.highlightsList}>
+                      {elem}
+                    </li>
+                  );
+                })
+              ) : (
+                <div className={styles.empty}>
+                  <img src={emptyImage} alt='Empty Item' />
+                  <p>No highlights for this product!</p>
+                </div>
+              )}
+            </ul>
           </div>
-        ) : (
-          ''
-        )}
-      </section>
-    </div>
+          {
+            <div className={styles.specificationBox}>
+              <h4>Specification</h4>
+              <hr />
+              {productDetails.specs.length ? (
+                productDetails.specs.map((element, i) => {
+                  return element.details ? (
+                    <div key={i}>
+                      <h5>{element.title}</h5>
+                      {element.details.map((elem, index) => {
+                        return (
+                          <div key={index} className={styles.property}>
+                            <div>
+                              <span>{elem.property}</span>
+                            </div>
+                            <div>
+                              <span>{elem.value}</span>
+                            </div>
+                          </div>
+                        );
+                      })}
+                      <hr />
+                    </div>
+                  ) : (
+                    ''
+                  );
+                })
+              ) : (
+                <div className={styles.empty}>
+                  <img src={emptyImage} alt='Empty Item' />
+                  <p>No specification for this product!</p>
+                </div>
+              )}
+            </div>
+          }
+        </section>
+      </div>
+    )
   );
 };
 
