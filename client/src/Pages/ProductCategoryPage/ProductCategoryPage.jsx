18bd10539031b356cfa41151b9f15cdcbdbd7f3f
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-13 00:36:09 +0530

    Added filter componet here

--- client/src/Pages/ProductCategoryPage/ProductCategoryPage.jsx
+++ client/src/Pages/ProductCategoryPage/ProductCategoryPage.jsx
@@ -1,13 +1,15 @@
 import {useEffect} from 'react';
 import styles from './ProductCategoryPage.module.css';
-import {useParams} from 'react-router-dom';
+import {useParams,useLocation} from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
 import {getProductsByCategory} from '../../Services/Actions/ProductAction';
 import ShowProducts from '../../Components/ShowProducts/ShowProducts';
 import SubHeader from '../../Components/SubHeader/SubHeader';
+import FilterBar from '../../Components/FilterBar/FilterBar';
 
 const ProductCategoryPage = () => {
   const {category} = useParams();
+  const location = useLocation();
   const dispatch = useDispatch();
   const {categoryProducts, offerProducts} = useSelector(
     (state) => state.ProductReducer
@@ -20,11 +22,14 @@ const ProductCategoryPage = () => {
   return (
     <div className={styles.productCategoryContainer}>
       <SubHeader />
-      {category === 'topoffers' ? (
-        <ShowProducts products={offerProducts} />
-      ) : (
-        <ShowProducts products={categoryProducts} />
-      )}
+      <div className={styles.categoryBox}>
+        <FilterBar />
+        {category === 'topoffers' ? (
+          <ShowProducts products={offerProducts} />
+        ) : (
+          <ShowProducts products={categoryProducts} />
+        )}
+      </div>
     </div>
   );
 };
