1c55b828124c515ea31b533e5649e07e223488b9
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-13 00:34:16 +0530

    Added path name at the top or shoproduct components

--- client/src/Components/ShowProducts/ShowProducts.jsx
+++ client/src/Components/ShowProducts/ShowProducts.jsx
@@ -1,15 +1,10 @@
 import {useDispatch, useSelector} from 'react-redux';
-import {Link, useNavigate} from 'react-router-dom';
+import {Link, useNavigate, useLocation} from 'react-router-dom';
 import styles from './ShowProducts.module.css';
-import {
-  addToWishlist,
-  removeFromWishlist,
-} from '../../Services/Actions/WishlistAction';
 import fAssured from '../../Assets/Images/f-assured.png';
 import {AiOutlineHeart, AiFillHeart} from 'react-icons/ai';
 import {FaShoppingCart} from 'react-icons/fa';
 import {GiElectric} from 'react-icons/gi';
-import {addToCart} from '../../Services/Actions/CartAction';
 import {
   addCart,
   addWishlist,
@@ -19,99 +14,107 @@ import {
 const ShowProducts = ({products}) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const {pathname} = useLocation();
   const {user} = useSelector((state) => state.UserReducer);
   const wishlist = useSelector((state) => state.WishlistReducer);
   const cart = useSelector((state) => state.CartReducer);
+  const product = useSelector((state) => state.ProductReducer);
+  // console.log(product.isLoading);
 
   return (
     <div className={styles.showProductsContainer}>
-      {products.map((elem) => {
-        // Calculating discount percent
-        const discount = Math.floor(
-          100 - (elem.current_price * 100) / elem.original_price
-        );
+      <p>Home{pathname.split('/').join(' > ')}</p>
+      <div className={styles.productsContainer}>
+        {products.map((elem) => {
+          // Calculating discount percent
+          const discount = Math.floor(
+            100 - (elem.current_price * 100) / elem.original_price
+          );
 
-        // Checking the product is added in wishlist or not
-        const isExistInWishlist = wishlist.wishlistData.filter(
-          (element) =>
-            element.productId === elem._id && element.userId === user.userId
-        );
+          // Checking the product is added in wishlist or not
+          const isExistInWishlist = wishlist.wishlistData.filter(
+            (element) =>
+              element.productId === elem._id && element.userId === user.userId
+          );
 
-        // Checking the product is added in cart or not
-        const isExistInCart = cart.cartData.filter(
-          (element) =>
-            element.productId === elem._id && element.userId === user.userId
-        );
+          // Checking the product is added in cart or not
+          const isExistInCart = cart.cartData.filter(
+            (element) =>
+              element.productId === elem._id && element.userId === user.userId
+          );
 
-        return (
-          <div key={elem._id} className={styles.productBox}>
-            {isExistInWishlist.length ? (
-              <AiFillHeart
-                className={styles.removeWishlistIcon}
-                onClick={() =>
-                  removeWishlist(dispatch, isExistInWishlist[0]._id)
-                }
-              />
-            ) : (
-              <AiOutlineHeart
-                onClick={() => addWishlist(dispatch, elem, user)}
-                className={styles.addWishlistIcon}
-                style={{stroke: 'silver', strokeWidth: '50'}}
-              />
-            )}
-            <div className={styles.productImageDiv}>
-              <Link to={`/productdetail/${elem._id}`}>
+          return (
+            <div key={elem._id} className={styles.productBox}>
+              {isExistInWishlist.length ? (
+                <AiFillHeart
+                  className={styles.removeWishlistIcon}
+                  onClick={() =>
+                    removeWishlist(dispatch, isExistInWishlist[0]._id)
+                  }
+                />
+              ) : (
+                <AiOutlineHeart
+                  onClick={() => addWishlist(dispatch, elem, user)}
+                  className={styles.addWishlistIcon}
+                  style={{stroke: 'silver', strokeWidth: '50'}}
+                />
+              )}
+              <div className={styles.productImageDiv}>
+                <Link to={`/productdetail/${elem._id}`}>
+                  <img
+                    src={elem.thumbnail}
+                    alt={elem.category}
+                    className={styles.productImage}
+                  />
+                </Link>
+              </div>
+              <Link
+                to={`/productdetail/${elem._id}`}
+                className={styles.productTitle}
+              >
+                <p className={styles.productName}>{elem.name}</p>
                 <img
-                  src={elem.thumbnail}
-                  alt={elem.category}
-                  className={styles.productImage}
+                  className={styles.fAssured}
+                  src={fAssured}
+                  alt='Flipkart assured'
                 />
               </Link>
-            </div>
-            <Link
-              to={`/productdetail/${elem._id}`}
-              className={styles.productTitle}
-            >
-              <p className={styles.productName}>{elem.name}</p>
-              <img
-                className={styles.fAssured}
-                src={fAssured}
-                alt='Flipkart assured'
-              />
-            </Link>
-            <p className={styles.price}>
-              <span className={styles.currentPrice}>₹{elem.current_price}</span>{' '}
-              <strike className={styles.originalPrice}>
-                ₹{elem.original_price}
-              </strike>
-              <span className={styles.discount}>{discount}% off</span>
-            </p>
-            <div className={styles.buttonDiv}>
-              {isExistInCart.length ? (
-                <button
-                  className={styles.addToCartBtn}
-                  onClick={() => navigate('/cart')}
-                >
-                  <FaShoppingCart />
-                  Go to Cart
+              <p className={styles.price}>
+                <span className={styles.currentPrice}>
+                  ₹{elem.current_price}
+                </span>{' '}
+                <strike className={styles.originalPrice}>
+                  ₹{elem.original_price}
+                </strike>
+                <span className={styles.discount}>{discount}% off</span>
+              </p>
+              <div className={styles.buttonDiv}>
+                {isExistInCart.length ? (
+                  <button
+                    className={styles.addToCartBtn}
+                    onClick={() => navigate('/cart')}
+                  >
+                    <FaShoppingCart />
+                    Go to Cart
+                  </button>
+                ) : (
+                  <button
+                    className={styles.addToCartBtn}
+                    onClick={() => addCart(dispatch, elem, user)}
+                  >
+                    <FaShoppingCart />
+                    Add to Cart
+                  </button>
+                )}
+                <button className={styles.buyNowBtn}>
+                  <GiElectric />
+                  Buy Now
                 </button>
-              ) : (
-                <button
-                  className={styles.addToCartBtn}
-                  onClick={() => addCart(dispatch, elem, user)}
-                >
-                  <FaShoppingCart />
-                  Add to Cart
-                </button>
-              )}
-              <button className={styles.buyNowBtn}>
-                <GiElectric />
-                Buy Now
-              </button>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 };
