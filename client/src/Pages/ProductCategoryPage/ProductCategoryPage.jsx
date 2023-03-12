b05888fc223638792a69576e79191e046f159583
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-12 20:43:32 +0530

    Update product category page with top offer products

--- client/src/Pages/ProductCategoryPage/ProductCategoryPage.jsx
+++ client/src/Pages/ProductCategoryPage/ProductCategoryPage.jsx
@@ -9,16 +9,22 @@ import SubHeader from '../../Components/SubHeader/SubHeader';
 const ProductCategoryPage = () => {
   const {category} = useParams();
   const dispatch = useDispatch();
-  const {categoryProducts} = useSelector((state) => state.ProductReducer);
+  const {categoryProducts, offerProducts} = useSelector(
+    (state) => state.ProductReducer
+  );
 
   useEffect(() => {
-    getProductsByCategory(dispatch, category);
+    category !== 'topoffers' && getProductsByCategory(dispatch, category);
   }, [category]);
 
   return (
     <div className={styles.productCategoryContainer}>
       <SubHeader />
-      <ShowProducts products={categoryProducts} />
+      {category === 'topoffers' ? (
+        <ShowProducts products={offerProducts} />
+      ) : (
+        <ShowProducts products={categoryProducts} />
+      )}
     </div>
   );
 };
