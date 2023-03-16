1955fc8a36b6fc695b4c91b2cd95e187b4546def
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-17 00:00:50 +0530

    Crated four section in checkout page

--- client/src/Pages/CheckoutPage/CheckoutPage.jsx
+++ client/src/Pages/CheckoutPage/CheckoutPage.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import {useSelector} from 'react-redux';
+import styles from './CheckoutPage.module.css';
 
 const CheckoutPage = () => {
+  const {user} = useSelector((state) => state.UserReducer);
+  const products = JSON.parse(localStorage.getItem('checkout')) || [];
   return (
-    <div>CheckoutPage</div>
-  )
-}
+    <div className={styles.checkoutContainer}>
+      <section className={styles.userDetails}>
+        <p>Login</p>
+        <p>
+          {user.first_name} {user.last_name} +91{user.mobile_number}
+        </p>
+      </section>
+      <section>
+        <p>DELEVERY ADDRESS</p>
+        <p>{products[0].name}</p>
+      </section>
+      <section>ORDER SUMMARY</section>
+      <section>PAYMENT OPTIONS</section>
+    </div>
+  );
+};
 
-export default CheckoutPage
+export default CheckoutPage;
