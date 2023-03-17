88691ba0d60266d764375f2b1ef84fd0bc03a1c0
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-17 16:57:07 +0530

    Added addAddress function in address action

--- client/src/Services/Actions/AddressAction.js
+++ client/src/Services/Actions/AddressAction.js
@@ -6,3 +6,14 @@ export const ADD_ADDRESS = 'ADD_ADDRESS';
 export const UPDATE_ADDRESS = 'UPDATE_ADDRESS';
 export const IS_LOADING_IN_ADDRESS = 'IS_LOADING_IN_ADDRESS';
 export const IS_ERROR_IN_ADDRESS = 'IS_ERROR_IN_ADDRESS';
+
+// Add address
+export const addAddress = async (dispatch, address) => {
+  dispatch({type: IS_LOADING_IN_ADDRESS});
+  try {
+    const response = await axios.post(`http://8080/api/addaddress`, address);
+    dispatch({type: ADD_ADDRESS, payload: response.data});
+  } catch (error) {
+    dispatch({type: IS_ERROR_IN_ADDRESS, payload: error});
+  }
+};
