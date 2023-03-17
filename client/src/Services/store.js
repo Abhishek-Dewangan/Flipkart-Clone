931ab911f1d9a41b76da92995c15691e505de16f
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-17 17:55:39 +0530

    Combined address reducer with root reducer

--- client/src/Services/store.js
+++ client/src/Services/store.js
@@ -3,12 +3,14 @@ import {ProductReducer} from './Reducers/ProductReducer';
 import {UserReducer} from './Reducers/UserReducer';
 import {WishlistReducer} from './Reducers/WishlistReducer';
 import {CartReducer} from './Reducers/CartReducer';
+import {AddressReducer} from './Reducers/AddressReducer';
 
 const rootReducer = combineReducers({
   ProductReducer,
   UserReducer,
   WishlistReducer,
   CartReducer,
+  AddressReducer,
 });
 
 export const store = createStore(rootReducer);
