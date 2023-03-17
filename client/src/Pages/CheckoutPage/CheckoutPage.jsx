4ea1630cf31c94c8bd9f8be199033330a7819c20
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-17 22:01:42 +0530

    Added sidebar for price details

--- client/src/Pages/CheckoutPage/CheckoutPage.jsx
+++ client/src/Pages/CheckoutPage/CheckoutPage.jsx
@@ -1,35 +1,48 @@
 import React from 'react';
 import {Button} from 'react-bootstrap';
 import {useSelector} from 'react-redux';
+import CartSidebar from '../CartPage/CartSidebar';
 import styles from './CheckoutPage.module.css';
 
 const CheckoutPage = ({handleShowAddress}) => {
   const {user} = useSelector((state) => state.UserReducer);
+  const {addressData} = useSelector((state) => state.AddressReducer);
+  console.log(addressData);
   const products = JSON.parse(localStorage.getItem('checkout')) || [];
   return (
     <div className={styles.checkoutContainer}>
-      <section className={styles.userDetails}>
-        <p>Login</p>
-        <p>
-          {user.first_name} {user.last_name} +91{user.mobile_number}
-        </p>
-      </section>
-      <section>
-        <p>DELEVERY ADDRESS</p>
-        <Button variant='outline-primary' onClick={handleShowAddress}>
-          Add Address
-        </Button>
-        <div></div>
-      </section>
-      <section>
-        <p>ORDER SUMMARY</p>
-        <div>
-          {products.map((elem) => {
-            return <div>{elem.name}</div>;
-          })}
-        </div>
-      </section>
-      <section>PAYMENT OPTIONS</section>
+      <div className={styles.orderSummaryBox}>
+        <section className={styles.userDetails}>
+          <p>Login</p>
+          <p>
+            {user.first_name} {user.last_name} +91{user.mobile_number}
+          </p>
+        </section>
+        <section className={styles.deleveryAddress}>
+          <p>DELEVERY ADDRESS</p>
+          {addressData.length &&
+            addressData.map((elem) => {
+              return <div>{elem.houseAddress}</div>;
+            })}
+
+          <Button variant='outline-primary' onClick={handleShowAddress}>
+            Add Address
+          </Button>
+          <div></div>
+        </section>
+        <section className={styles.orderSummary}>
+          <p>ORDER SUMMARY</p>
+          <div>
+            {products.map((elem, i) => {
+              return <div key={i}>{elem.name}</div>;
+            })}
+          </div>
+        </section>
+        <section className={styles.paymentOptions}>PAYMENT OPTIONS</section>
+      </div>
+      <div className={styles.priceDetailBox}>
+        <CartSidebar cartData={products} />
+      </div>
     </div>
   );
 };
