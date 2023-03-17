7a8735169c5b533d92a437efd56579c995007957
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-17 11:40:13 +0530

    Address aciton created with all action types

---
+++ client/src/Services/Actions/AddressAction.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+// Address action types
+export const GET_ADDRESS = 'GET_ADDRESS';
+export const ADD_ADDRESS = 'ADD_ADDRESS';
+export const UPDATE_ADDRESS = 'UPDATE_ADDRESS';
+export const IS_LOADING_IN_ADDRESS = 'IS_LOADING_IN_ADDRESS';
+export const IS_ERROR_IN_ADDRESS = 'IS_ERROR_IN_ADDRESS';
