9e7d85032fad124cd8644f6a416b93dec1dc69f6
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-26 00:04:12 +0530

    Order action and its type of operation is created

---
+++ client/src/Services/Actions/OrderAction.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+// Orders action types
+export const IS_LOADING_IN_ORDER = 'IS_LOADING_IN_ORDER';
+export const IS_ERROR_IN_ORDER = 'IS_ERROR_IN_ORDER';
+export const ADD_ORDER = 'ADD_ORDER';
+export const GET_ORDERS = 'GET_ORDERS';
