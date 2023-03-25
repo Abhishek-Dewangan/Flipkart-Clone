faac752ab912b73c1ab3b3e6fc674b2725482106
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-26 00:08:53 +0530

    Add order function is added in order action

--- client/src/Services/Actions/OrderAction.js
+++ client/src/Services/Actions/OrderAction.js
@@ -5,3 +5,18 @@ export const IS_LOADING_IN_ORDER = 'IS_LOADING_IN_ORDER';
 export const IS_ERROR_IN_ORDER = 'IS_ERROR_IN_ORDER';
 export const ADD_ORDER = 'ADD_ORDER';
 export const GET_ORDERS = 'GET_ORDERS';
+
+// Add order
+export const addOrder = async (dispatch, products) => {
+  dispatch({type: IS_LOADING_IN_ORDER});
+  try {
+    const res = await axios.post(
+      'http://localhost:8080/api/addorder',
+      products
+    );
+    console.log(res.data);
+    dispatch({type: ADD_ORDER, payload: res.data});
+  } catch (error) {
+    dispatch({type: IS_ERROR_IN_ORDER, payload: error.response.data});
+  }
+};
