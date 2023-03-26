50db611db96f48abdcd6f93b305ce201a0d1b0f3
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-27 01:16:07 +0530

    Show ordered successfully model created

--- client/src/Components/OrderSuccess/OrderSuccess.jsx
+++ client/src/Components/OrderSuccess/OrderSuccess.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import { Modal } from 'react-bootstrap'
-import styles from './OrderSuccess.module.css'
+import React from 'react';
+import {Modal} from 'react-bootstrap';
+import styles from './OrderSuccess.module.css';
 
-const OrderSuccess = ({OrderSuccess,handleCloseOrderSuccess}) => {
+const OrderSuccess = ({showOrderSuccess, handleCloseOrderSuccess}) => {
   return (
-    <Modal show={OrderSuccess}>OrderSuccess</Modal>
-  )
-}
+    <Modal show={showOrderSuccess} onHide={handleCloseOrderSuccess}>
+      <Modal.Header closeButton>
+        <Modal.Title>Modal heading</Modal.Title>
+      </Modal.Header>
+      <Modal.Body></Modal.Body>
+    </Modal>
+  );
+};
 
-export default OrderSuccess
+export default OrderSuccess;
