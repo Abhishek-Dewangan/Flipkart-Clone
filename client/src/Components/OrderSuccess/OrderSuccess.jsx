e288c625e917c5db9234acdca24199ae331ab1ad
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-03-27 02:14:26 +0530

    Order success modal is designed successfully with all need functionality

--- client/src/Components/OrderSuccess/OrderSuccess.jsx
+++ client/src/Components/OrderSuccess/OrderSuccess.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
-import {Modal} from 'react-bootstrap';
+import {Button, Modal} from 'react-bootstrap';
 import styles from './OrderSuccess.module.css';
+import {BsFillCheckCircleFill} from 'react-icons/bs';
+import {useNavigate} from 'react-router-dom';
 
 const OrderSuccess = ({showOrderSuccess, handleCloseOrderSuccess}) => {
+  const navigate = useNavigate();
   return (
-    <Modal show={showOrderSuccess} onHide={handleCloseOrderSuccess}>
-      <Modal.Header closeButton>
-        <Modal.Title>Modal heading</Modal.Title>
-      </Modal.Header>
-      <Modal.Body></Modal.Body>
+    <Modal
+      size='sm'
+      show={showOrderSuccess}
+      onHide={handleCloseOrderSuccess}
+      className={styles.orderSuccessModal}
+    >
+      <Modal.Body as={'div'} className={styles.body}>
+        <div className={styles.iconDiv}>
+          <BsFillCheckCircleFill className={styles.successIcon} />
+        </div>
+        <p>Ordered Successful</p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button
+          className={styles.continueShoppingBtn}
+          variant=''
+          onClick={() => {
+            handleCloseOrderSuccess();
+            navigate('/');
+          }}
+        >
+          Continue Shopping
+        </Button>
+      </Modal.Footer>
     </Modal>
   );
 };
