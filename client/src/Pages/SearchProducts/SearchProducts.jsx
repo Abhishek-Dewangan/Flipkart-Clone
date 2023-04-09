433182c66598c2a3ee33365f89cc6b62e937a310
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-04-10 00:32:27 +0530

    Added empty image if searched product in not found

--- client/src/Pages/SearchProducts/SearchProducts.jsx
+++ client/src/Pages/SearchProducts/SearchProducts.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import ShowProducts from "../../Components/ShowProducts/ShowProducts";
 import { searchProducts } from "../../Services/Actions/ProductAction";
+import emptyImage from "../../Assets/Images/empty.png";
 
 const SearchProducts = () => {
   const { query } = useParams();
@@ -16,11 +17,19 @@ const SearchProducts = () => {
 
   return (
     <div className={styles.searchProductsContainer}>
-      <ShowProducts
-        products={product.searchProducts}
-        isLoading={product.isLoading}
-        isSuccess={product.isSuccess}
-      />
+      {product.searchProducts.length ? (
+        <ShowProducts
+          products={product.searchProducts}
+          isLoading={product.isLoading}
+          isSuccess={product.isSuccess}
+        />
+      ) : (
+        <div className={styles.emptyWishlist}>
+          <img src={emptyImage} alt="Empty Item" />
+          <h4>Not Found</h4>
+          <p>Not matching any products!</p>
+        </div>
+      )}
     </div>
   );
 };
