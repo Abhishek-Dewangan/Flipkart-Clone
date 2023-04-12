eee82e46d0cbda3ac22b772cd805d728db72297c
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-04-13 01:23:30 +0530

    Update the data base

--- routes/router.js
+++ routes/router.js
@@ -1,73 +1,73 @@
-const express = require('express');
+const express = require("express");
 const {
   getAddress,
   addNewAddress,
   removeAddress,
   updateAddress,
-} = require('../controllers/AddressController');
+} = require("../controllers/AddressController");
 const {
   addToCart,
   removeFromCart,
   getCartProducts,
   removeAllFromCart,
   removeMultipleFromCart,
-} = require('../controllers/CartController');
-const {getOrders, addOrder} = require('../controllers/OrderController');
+} = require("../controllers/CartController");
+const { getOrders, addOrder } = require("../controllers/OrderController");
 const {
   getProducts,
   getProductById,
   getProductsByCategory,
   getOfferProducts,
-} = require('../controllers/ProductController');
+} = require("../controllers/ProductController");
 const {
   signin,
   signup,
   signout,
   authentication,
-} = require('../controllers/UserController');
+} = require("../controllers/UserController");
 const {
   addToWishlist,
   removeFromWishlist,
   getWishlistProducts,
   removeAllFromWishlist,
-} = require('../controllers/WishlistController');
+} = require("../controllers/WishlistController");
 
 const router = express.Router();
 
 // User routes
-router.post('/signup', signup);
-router.post('/signin', signin);
-router.post('/signout', signout);
-router.post('/authentication', authentication);
+router.post("/signup", signup);
+router.post("/signin", signin);
+router.post("/signout", signout);
+router.post("/authentication", authentication);
 
 // Product routes
-router.get('/getproducts', getProducts);
-router.get('/getofferproducts', getOfferProducts);
-router.get('/getproductbyid/:id', getProductById);
-router.get('/getproductsbycategory/:category', getProductsByCategory);
+router.get("/getproducts", getProducts);
+router.get("/getofferproducts", getOfferProducts);
+router.get("/getproductbyid/:id", getProductById);
+router.get("/getproductsbycategory/:category", getProductsByCategory);
 
 // Cart routes
-router.get('/getcartproducts/:userid', getCartProducts);
-router.post('/addtocart', addToCart);
-router.delete('/removefromcart/:id', removeFromCart);
-router.delete('/removeallfromcart/:userid', removeAllFromCart);
-router.delete('/removemultiplefromcart/:userid', removeMultipleFromCart);
+router.get("/getcartproducts/:userid", getCartProducts);
+router.post("/addtocart", addToCart);
+router.delete("/removefromcart/:id", removeFromCart);
+router.delete("/removeallfromcart/:userid", removeAllFromCart);
+router.delete("/removemultiplefromcart/:userid", removeMultipleFromCart);
 
 // Wishlist routes
-router.get('/getwishlistproducts/:userid', getWishlistProducts);
-router.post('/addtowishlist', addToWishlist);
-router.delete('/removefromwishlist/:id', removeFromWishlist);
-router.delete('/removeallfromwishlist/:userid', removeAllFromWishlist);
+router.get("/getwishlistproducts/:userid", getWishlistProducts);
+router.post("/addtowishlist", addToWishlist);
+router.delete("/removefromwishlist/:id", removeFromWishlist);
+router.delete("/removeallfromwishlist/:userid", removeAllFromWishlist);
 
 // Address routes
-router.get('/getaddress/:userid', getAddress);
-router.post('/addnewaddress', addNewAddress);
-router.put('/updateaddress/:id', updateAddress);
-router.delete('/removeaddress/:id', removeAddress);
+router.get("/getaddress/:userid", getAddress);
+router.post("/addnewaddress", addNewAddress);
+router.put("/updateaddress/:id", updateAddress);
+router.delete("/removeaddress/:id", removeAddress);
 
 // Orders routes
-router.get('/getorders/:id', getOrders);
-router.post('/addorder', addOrder);
+router.get("/getorders/:id", getOrders);
+router.post("/addorder", addOrder);
 
 // Exporting router
 module.exports = router;
