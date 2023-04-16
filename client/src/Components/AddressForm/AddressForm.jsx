fd540b3e2901605dc570520c5ad9ebefd60e43c2
Abhshek-Dewangan <callmeabhi1997@gmail.com>
2023-04-17 00:18:03 +0530

    worked on code cleaning

--- client/src/Components/AddressForm/AddressForm.jsx
+++ client/src/Components/AddressForm/AddressForm.jsx
@@ -1,7 +1,7 @@
-import styles from "./AddressForm.module.css";
-import { Modal } from "react-bootstrap";
-import { addAddress } from "../../Services/Actions/AddressAction";
-import { useDispatch, useSelector } from "react-redux";
+import styles from './AddressForm.module.css';
+import { Modal } from 'react-bootstrap';
+import { addAddress } from '../../Services/Actions/AddressAction';
+import { useDispatch, useSelector } from 'react-redux';
 
 const AddressForm = ({ show, handleCloseAddress }) => {
   const dispatch = useDispatch();
@@ -25,66 +25,72 @@ const AddressForm = ({ show, handleCloseAddress }) => {
   return (
     <div className={styles.addressContainer}>
       <Modal
-        size=""
+        size=''
         show={show}
         onHide={handleCloseAddress}
-        className={styles.modal}
-      >
+        className={styles.modal}>
         <div className={styles.formDiv}>
           <Modal.Header closeButton>
             <Modal.Title className={styles.formTitle}>ADD ADDRESS</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <form onSubmit={submit} className={styles.addressForm}>
+            <form
+              onSubmit={submit}
+              className={styles.addressForm}>
               <div>
-                <input required type={"text"} placeholder="Name" name="name" />{" "}
                 <input
                   required
-                  type={"number"}
-                  placeholder="10-digit mobile number"
-                  name="mobile_number"
+                  type={'text'}
+                  placeholder='Name'
+                  name='name'
+                />{' '}
+                <input
+                  required
+                  type={'number'}
+                  placeholder='10-digit mobile number'
+                  name='mobile_number'
                 />
               </div>
               <div>
                 <input
-                  type={"number"}
-                  placeholder={"Pincode"}
-                  name={"pincode"}
+                  type={'number'}
+                  placeholder={'Pincode'}
+                  name={'pincode'}
                   required
                 />
                 <input
-                  type={"text"}
-                  placeholder={"Locality"}
-                  name={"locality"}
+                  type={'text'}
+                  placeholder={'Locality'}
+                  name={'locality'}
                 />
               </div>
               <div>
                 <input
-                  type={"text"}
-                  placeholder={"Address (Area and Street)"}
-                  name={"houseAddress"}
+                  type={'text'}
+                  placeholder={'Address (Area and Street)'}
+                  name={'houseAddress'}
                   required
                 />
               </div>
               <div>
                 <input
-                  type={"text"}
-                  placeholder={"City/District/Town"}
-                  name={"city"}
+                  type={'text'}
+                  placeholder={'City/District/Town'}
+                  name={'city'}
                   required
                 />
                 <input
-                  type={""}
-                  placeholder={"State"}
-                  name={"state"}
+                  type={''}
+                  placeholder={'State'}
+                  name={'state'}
                   required
                 />
               </div>
               <div>
                 <input
                   className={styles.submitBtn}
-                  type={"submit"}
-                  value="ADD ADDRESS"
+                  type={'submit'}
+                  value='ADD ADDRESS'
                 />
               </div>
             </form>
